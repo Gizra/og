df92e6b3d653605cf163ebe6b9a6435e9d911991
Gerhard Killesreiter <killes_www_drop_org@227.no-reply.drupal.org>
2005-10-20 10:20:30 +0000

    Update script and README

---
+++ README_update.txt
@@ -0,0 +1,1 @@
+Use the update.og.php script if you are upgrading from a og installation from before 2005-10-19.
---
+++ update-og.php
@@ -0,0 +1,13 @@
+<?php
+// $Id$
+include_once "includes/bootstrap.inc";
+drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
+
+db_query("ALTER TABLE `og_uid` ADD `og_role` int(1) NOT NULL default '0'");
+
+$result = db_query("SELECT * FROM {node_access} WHERE realm = 'og_uid' AND grant_view = 1");
+while ($object = db_fetch_object($result)) {
+  $sql = "REPLACE INTO {og_uid} (nid, uid, og_role) VALUES (%d, %d, %d)";
+  db_query($sql, $gid, $uid, ($object->grant_view + $object->grant_update));
+}
+?>
