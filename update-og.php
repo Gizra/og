d2b118189c26d282861e4394d3a536cdaf795f1f
moshe weitzman <weitzman@cyrve.com>
2006-02-11 03:12:31 +0000

    bug fix for an update script. minor improvement to output of group mission statement

--- update-og.php
+++ update-og.php
@@ -9,8 +9,8 @@ db_query("ALTER TABLE `og_uid` ADD `is_admin` int(1) default '0'");
 
 $result = db_query("SELECT * FROM {node_access} WHERE realm = 'og_uid' AND grant_view = 1");
 while ($object = db_fetch_object($result)) {
-  $sql = "REPLACE INTO {og_uid} (nid, uid, og_role) VALUES (%d, %d, %d)";
-  db_query($sql, $gid, $uid, ($object->grant_view + $object->grant_update));
+  $sql = "REPLACE INTO {og_uid} (nid, uid, og_role, is_admin, is_active) VALUES (%d, %d, %d, %d, 1)";
+  db_query($sql, $object->nid, $object->gid, ($object->grant_view + $object->grant_update), $object->grant_update);
 }
 
 ?>
