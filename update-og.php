832f784688913d8bb347601e3c94f61d38741382
moshe weitzman <weitzman@cyrve.com>
2006-02-11 23:46:12 +0000

    preserve subscription requests during the old update

--- update-og.php
+++ update-og.php
@@ -7,10 +7,10 @@ db_query("ALTER TABLE `og_uid` ADD `og_role` int(1) NOT NULL default '0'");
 db_query("ALTER TABLE `og_uid` ADD `is_active` int(1) default '0'");
 db_query("ALTER TABLE `og_uid` ADD `is_admin` int(1) default '0'");
 
-$result = db_query("SELECT * FROM {node_access} WHERE realm = 'og_uid' AND grant_view = 1");
+$result = db_query("SELECT * FROM {node_access} WHERE realm = 'og_uid'");
 while ($object = db_fetch_object($result)) {
-  $sql = "REPLACE INTO {og_uid} (nid, uid, og_role, is_admin, is_active) VALUES (%d, %d, %d, %d, 1)";
-  db_query($sql, $object->nid, $object->gid, ($object->grant_view + $object->grant_update), $object->grant_update);
+  $sql = "REPLACE INTO {og_uid} (nid, uid, og_role, is_admin, is_active) VALUES (%d, %d, %d, %d, %d)";
+  db_query($sql, $object->nid, $object->gid, ($object->grant_view + $object->grant_update), $object->grant_update, $object->grant_view);
 }
 
 ?>
