5dd5f1cd7f5c553e576f86ad470f519a4aeb46d5
moshe weitzman <weitzman@cyrve.com>
2006-02-20 06:21:51 +0000

    #49179 public posts should be shown on group home page to non subscribers

--- og-update-20060206.php
+++ og-update-20060206.php
@@ -21,4 +21,27 @@ while ($row = db_fetch_object($result)) {
   db_queryd($sql, $row->nid, $row->nid);
 }
 
-?>
+// feb 19, 2006
+// add a row for each combination of public node and group. needed to make public nodes show up in group homepage for non subscribers
+$sql = "SELECT DISTINCT(nid) as nid FROM {node_access} WHERE realm = 'og_group' AND gid = 0" ;
+$result = db_queryd($sql);
+while ($row = db_fetch_object($result)) {
+  $sql = "SELECT gid FROM {node_access} WHERE nid = %d AND realm = 'og_group' AND gid != 0" ;
+  $result2 = db_queryd($sql, $row->nid);
+  while ($row2 = db_fetch_object($result2)) {  
+    $sql = "REPLACE INTO {node_access} (nid, realm, gid, grant_view) VALUE (%d, 'og_public', 0, %d)";
+    db_queryd($sql, $row->nid, $row2->gid); 
+  }
+}
+
+// change all former public node grants to 'og_all' realm
+$sql = "UPDATE {node_access} SET realm = 'og_all' WHERE realm = 'og_group' AND gid = 0 AND grant_view = 1";
+db_queryd($sql);
+
+// change all nodes in groups to new 'og_subscriber' realm
+$sql = "UPDATE {node_access} SET realm = 'og_subscriber' WHERE realm = 'og_group' AND gid != 0";
+db_queryd($sql);
+
+// these records are no longer used. we've migrated them to new grant scheme
+$sql = "DELETE FROM {node_access} WHERE realm = 'og_group'";
+db_queryd($sql);
