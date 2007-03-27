fea76740d93a64c154f38068ca69602ebbcd179c
moshe weitzman <weitzman@cyrve.com>
2007-03-27 03:26:12 +0000

    #131197 better og.js. patch by jhm ... also fix a NOTICE in og.module

--- og.js
+++ og.js
@@ -4,22 +4,31 @@ Drupal.ogAttach = function() {
 /*  Disable the public checkbox if no groups are selected in in Audience*/
   $('.og-audience').click(function() {
     // Audience can be select or checkboxes
-    if ($('input.og-audience.form-checkbox').size()) {
-      var cnt = $('input.og-audience:checked').size();  
+		var cnt;
+    if ( $('.og-audience .form-checkbox').size() > 0) {
+      cnt = $('input.og-audience:checked').size();  
     }
     else {
-      var cnt = $('select.og-audience option:selected').size();      
+      cnt = $('.og-audience option:selected').size();      
     }
-    
-      
     if (cnt > 0) {
       $('#edit-og-public').removeAttr("disabled");
     }
     else {
       $('#edit-og-public').attr("disabled", "disabled");
     }
-  })
-  $('.og-audience').click();
+  });
+
+	if ( $('.og-audience .form-checkbox').size() > 0 ) {
+	  if ( $('input.og-audience:checked').size() < 1) {
+	      $('#edit-og-public').attr("disabled", "disabled");
+	  }		
+	}
+	else {
+	  if ( $('.og-audience option:selected').size() < 1) {
+	      $('#edit-og-public').attr("disabled", "disabled");
+	  }				
+	}
 }
 
 if (Drupal.jsEnabled) {
