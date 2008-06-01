e65d25cde59b0b84deaafa4d8979d48d62a027ee
moshe weitzman <weitzman@cyrve.com>
2008-06-01 14:52:38 +0000

    reorganize js a bit

--- og_access.js
+++ og_access.js
@@ -1,5 +1,4 @@
-/* Node authoring form for group posts
- */
+// Node authoring form for group posts
 Drupal.behaviors.og_access_group_post = function() {
   
   /* Node authoring form for group content -Disable the public checkbox if no groups are selected in in Audience */
@@ -33,8 +32,12 @@ Drupal.behaviors.og_access_group_post = function() {
         $('#edit-og-public').attr("disabled", "disabled");
       }        
     }
+}
 
-    /* Node authoring form for group homepages - Don't allow "private group" and "Open subscription" at the same time 
+
+// Node authoring form for group homepages
+Drupal.behaviors.og_access_group_node = function() {
+    /* Don't allow "private group" and "Open subscription" at the same time 
      * This is just for improved UI. You may change it if you need this combination.
      */
     $("#edit-og-private").click(function() { 
@@ -59,13 +62,7 @@ Drupal.behaviors.og_access_group_post = function() {
     if ($("#edit-og-private:checked").val()) {
         $("input[@Name='og_selective']:nth(0)").removeAttr('checked').attr('disabled','disabled');
     }
-}
-
-
-/* Node authoring form for group homepages - Don't allow "private group" and "list in groups directory" at the same time 
- * This is just for improved UI. You may change it if you need this combination.
- */
-Drupal.behaviors.og_access_group_node = function() {
+    
     $("#edit-og-private").click(function() { 
       if ($("#edit-og-private:checked").val()) {
         $("#edit-og-directory").removeAttr("checked").attr('disabled','disabled');
@@ -75,6 +72,10 @@ Drupal.behaviors.og_access_group_node = function() {
       }
     });
 
+    /*
+     * Don't allow "private group" and "list in groups directory" at the same time 
+     * This is just for improved UI. You may change it if you need this combination.
+     */
     $("#edit-og-directory").click(function() {
       if ($("#edit-og-directory:checked").val()) {
         $("#edit-og-private").attr('disabled','disabled');
@@ -83,49 +84,22 @@ Drupal.behaviors.og_access_group_node = function() {
         $("#edit-og-private").removeAttr('disabled');
       }
     });
+    
     if ($("#edit-og-directory:checked").val() && !$("#edit-og-private:checked").val()) {
         $("#edit-og-private").attr('disabled','disabled');
     }
     if ($("#edit-og-private:checked").val() && !$("#edit-og-directory:checked").val()) {
         $("#edit-og-directory").attr('disabled','disabled');
     }
-  
-
-  /* admin og settings form, "Group details - Private Groups"
-   * Disable "always public" if Node authoring visibility set to "Visible only within the targeted groups"
-   * Disable "always private" if Node authoring visibility set to "Visible within the targeted groups and on other pages"
-   */
-  $("input[@Name='og_visibility']").click(function() {
-    if ($("input[@Name='og_visibility']:checked").val() == 0) {
-        $("input[@name='og_private_groups']:nth(0)").attr('disabled','disabled');
-        $("input[@name='og_private_groups']:nth(1)").removeAttr('disabled');
-      }
-      else if ($("input[@Name='og_visibility']:checked").val() == 1) {
-        $("input[@name='og_private_groups']:nth(0)").removeAttr('disabled');
-        $("input[@name='og_private_groups']:nth(1)").attr('disabled','disabled');
-      } 
-      else {
-        $("input[@name='og_private_groups']:nth(0)").removeAttr('disabled');
-        $("input[@name='og_private_groups']:nth(1)").removeAttr('disabled');
-      }
-    }
-  );
-
-  if ($("input[@Name='og_visibility']:checked").val() == 0) {
-      $("input[@name='og_private_groups']:nth(0)").attr('disabled','disabled');
-      $("input[@name='og_private_groups']:nth(1)").removeAttr('disabled');
-  }
-  else if ($("input[@Name='og_visibility']:checked").val() == 1) {
-      $("input[@name='og_private_groups']:nth(0)").removeAttr('disabled');
-      $("input[@name='og_private_groups']:nth(1)").attr('disabled','disabled');     
-  }
 }
+  
     
   /* admin og settings form, "Node Authoring Form - Visibilty of Posts"
    * Disable "Visible within the targeted groups and on other pages" if private groups set to "always private"
    * Disable "Visible only within the targeted groups" if private groups set to "always public"
    */
 Drupal.behaviors.og_access_admin_settings = function() {
+  console.log("hi");
   $("input[@Name='og_private_groups']").click(function() {
       if ( $("input[@Name='og_private_groups']:checked").val() == 1 ) {
         $("input[@name='og_visibility']:nth(0)").removeAttr('disabled');
@@ -142,6 +116,7 @@ Drupal.behaviors.og_access_admin_settings = function() {
     }
   );
 
+  // Set intial value.
   if ( $("input[@Name='og_private_groups']:checked").val() == 1 ) {
     $("input[@name='og_visibility']:nth(0)").removeAttr('disabled');
     $("input[@name='og_visibility']:nth(1)").attr('disabled','disabled');
@@ -150,4 +125,34 @@ Drupal.behaviors.og_access_admin_settings = function() {
       $("input[@name='og_visibility']:nth(0)").attr('disabled','disabled');
       $("input[@name='og_visibility']:nth(1)").removeAttr('disabled');  
   }
+  
+    /* "Group details - Private Groups"
+     * Disable "always public" if Node authoring visibility set to "Visible only within the targeted groups"
+     * Disable "always private" if Node authoring visibility set to "Visible within the targeted groups and on other pages"
+     */
+    $("input[@Name='og_visibility']").click(function() {
+      if ($("input[@Name='og_visibility']:checked").val() == 0) {
+          $("input[@name='og_private_groups']:nth(0)").attr('disabled','disabled');
+          $("input[@name='og_private_groups']:nth(1)").removeAttr('disabled');
+        }
+        else if ($("input[@Name='og_visibility']:checked").val() == 1) {
+          $("input[@name='og_private_groups']:nth(0)").removeAttr('disabled');
+          $("input[@name='og_private_groups']:nth(1)").attr('disabled','disabled');
+        } 
+        else {
+          $("input[@name='og_private_groups']:nth(0)").removeAttr('disabled');
+          $("input[@name='og_private_groups']:nth(1)").removeAttr('disabled');
+        }
+      }
+    );
+
+    // Set initial value.
+    if ($("input[@Name='og_visibility']:checked").val() == 0) {
+        $("input[@name='og_private_groups']:nth(0)").attr('disabled','disabled');
+        $("input[@name='og_private_groups']:nth(1)").removeAttr('disabled');
+    }
+    else if ($("input[@Name='og_visibility']:checked").val() == 1) {
+        $("input[@name='og_private_groups']:nth(0)").removeAttr('disabled');
+        $("input[@name='og_private_groups']:nth(1)").attr('disabled','disabled');     
+    }
 }
