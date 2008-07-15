2f1a21b0541c51567328d93314015c21d7baac3e
moshe weitzman <weitzman@cyrve.com>
2008-07-15 17:11:53 +0000

    #280501 by tmcw. SimpleTest tests are not updated for Drupal 6 head. tests are cleaner now, a bit more thorough, and almost all passing.

--- tests/og_testcase.php
+++ tests/og_testcase.php
@@ -4,62 +4,25 @@ class OgTestCase extends DrupalTestCase {
   var $_cleanupGroups;
   
   function get_info() {
-    return array();
+    return array('name'  => t('Og testing functionality'),
+                 'desc'  => t('Setup and teardown functionality for organic groups'),
+                 'group' => 'Organic groups');
   }
   
   function addOg($type, $selective = OG_OPEN) {
-    
     $edit = array();
-    $edit['title']          = '!SimpleTest test group node! ' . $this->randomName(10);
+    $title = '!SimpleTest test group node! ' . $this->randomName(10); 
+    $edit['title']          = $title;
     $edit['og_description'] = '!SimpleTest og description' . $this->randomName(10);
     $edit['body']           = '!SimpleTest test group welcome message! ' . $this->randomName(32) . ' ' . $this->randomName(32);
     $edit['og_selective'] = (string)$selective;
-//    $this->_browser->setFieldByName('edit[og_theme]', ''); // May not be present, so no error catching
-
-    $this->drupalPost("node/add/$type", $edit, t('Submit'));
-
-    $this->assertWantedRaw(t('Your %post has been created.', array ('%post' => $type)), 'Group created');
-
+    $this->drupalPost("node/add/".str_replace('_', '-', $type), $edit, t('Save'));
+    $this->assertWantedRaw(t('has been created', array ('%post' => $type, '%title' => $title)), 'Group created'); //TODO: make accurate again
     $node = node_load(array('title' => $edit['title']));
     $this->assertNotNull($node, 'Group found in database. %s');
     $this->_cleanupGroups[] = $node->nid;
-    
     return $node->nid;
   }
-  
-  // TODO: in D6, there is similar method in drupalTestCase
-  function addNodeType() {
-    
-    $type = new stdClass();
-
-    $name = strtolower($this->randomName());
-    $type->type = trim($name);
-    $type->name = trim($name);
-    $type->orig_type = trim("");
-    $type->old_type = $type->type;
-  
-    $type->description    = $this->randomName(32, "description ... ");
-    $type->help           = $type->description = $this->randomName(32, "help ... ");
-    $type->min_word_count = 0;
-    $type->title_label    = "Title";
-    $type->body_label     = "Body";
-  
-    $type->module         = 'node';
-    $type->has_title      = $type->has_body = TRUE;
-    $type->custom         = "";
-    $type->modified       = TRUE;
-    $type->locked         = TRUE;
-  
-    $status = node_type_save($type);
-    $this->assertTrue(SAVED_NEW == $status, "Created node-type $name.");
-    
-    $this->_cleanupNodeTypes[] = $name;
-    
-    variable_set('og_content_type_usage_'. $name, 'group');
-    
-    return $name;
-  }
-  
   
   function tearDown() {
     while (sizeof($this->_cleanupGroups) > 0) {
@@ -67,15 +30,6 @@ class OgTestCase extends DrupalTestCase {
       node_delete($gid);
     }
     
-    include_once './'. drupal_get_path('module', 'node') .'/content_types.inc';
-    while (sizeof($this->_cleanupNodeTypes) > 0) {
-      $name = array_pop($this->_cleanupNodeTypes);
-
-      $ar = array('name' => $name, 'type' => $name);
-      // named this array so it can be passed by reference.
-      // anonymous variables cannot.
-      node_type_delete_confirm_submit(0, $ar);
-    }
     parent::tearDown();
   }
 }
