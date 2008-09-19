3f8661a5f11c953ff35fe4ece31dd3246450aeac
moshe weitzman <weitzman@cyrve.com>
2008-09-19 16:56:33 +0000

    #310384. The posting and wiki tests will complete now, with errors. We need to fixn those up and update the subscription test some more. help wanted.

--- tests/og_testcase.php
+++ tests/og_testcase.php
@@ -1,13 +1,14 @@
 <?php
 
-class OgTestCase extends DrupalTestCase {
+// TODO: This  class is barely useful anymore.
+class OgTestCase extends DrupalWebTestCase {
   var $_cleanupGroups;
   
-  function get_info() {
-    return array('name'  => t('Og testing functionality'),
-                 'desc'  => t('Setup and teardown functionality for organic groups'),
-                 'group' => 'Organic groups');
-  }
+  // function getInfo() {
+  //     return array('name'  => t('Og testing functionality'),
+  //                  'desc'  => t('Setup and teardown functionality for organic groups'),
+  //                  'group' => 'Organic groups');
+  //   }
   
   function addOg($type, $selective = OG_OPEN) {
     $edit = array();
@@ -16,20 +17,7 @@ class OgTestCase extends DrupalTestCase {
     $edit['og_description'] = '!SimpleTest og description' . $this->randomName(10);
     $edit['body']           = '!SimpleTest test group welcome message! ' . $this->randomName(32) . ' ' . $this->randomName(32);
     $edit['og_selective'] = (string)$selective;
-    $this->drupalPost("node/add/".str_replace('_', '-', $type), $edit, t('Save'));
-    $this->assertWantedRaw(t('has been created', array ('%post' => $type, '%title' => $title)), 'Group created'); //TODO: make accurate again
-    $node = node_load(array('title' => $edit['title']));
-    $this->assertNotNull($node, 'Group found in database. %s');
-    $this->_cleanupGroups[] = $node->nid;
+    $node = $this->drupalCreateNode($edit);
     return $node->nid;
-  }
-  
-  function tearDown() {
-    while (sizeof($this->_cleanupGroups) > 0) {
-      $gid = array_pop($this->_cleanupGroups);
-      node_delete($gid);
-    }
-    
-    parent::tearDown();
   }
 }
