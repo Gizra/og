f0bbc3cbab3338eb7aff2a6a913f882a0bd26846
brice User <brice@ubuntu.ubuntu-domain>
2009-09-20 08:56:54 -0400

    Remove tabs tabs spaces.

--- og.api.php
+++ og.api.php
@@ -13,10 +13,10 @@
 
 /**
  * Alter a group that is being fetched.
- * 
+ *
  * @param $group
  *   An object with the following keys:
- *   - nid: 
+ *   - nid:
  *       The node ID of the group.
  *   - data:
  *       Optional; An array of data related to the association. The data is per
@@ -29,15 +29,15 @@ function hook_og_get_group_alter($group) {
     // An implementing module should act on this data and change the theme
     // accordingly.
     $group->data['theme'] = 'MY_THEME';
-  }  
+  }
 }
 
 /**
  * Alter a group that is being saved.
- * 
+ *
  * @param $group
  *   An object with the following keys:
- *   - nid: 
+ *   - nid:
  *       The node ID of the group.
  *   - data:
  *       Optional; An array of data related to the association. The data is per
@@ -49,9 +49,9 @@ function hook_og_set_group_alter($group) {
 
 /**
  * Return the types of group or group posts available.
- * 
+ *
  * @return
- *   An array keyed with the type name and it's value is an array with the 
+ *   An array keyed with the type name and it's value is an array with the
  *   following keys:
  *   - type: The type can be "group", "group_post" or "omitted".
  *   - description: Explanation about the type.
@@ -68,7 +68,7 @@ function hook_og_types_info() {
 
 /**
  * Alter the users which will be notified about a subscription of another user.
- * 
+ *
  * @param $uids
  *   An array with the users ID, passed by reference.
  * @param $node
@@ -86,17 +86,17 @@ function hook_og_user_request(&$uids, $node, $group, $account, $request) {
 }
 
 /**
- * Define selective types of groups. 
+ * Define selective types of groups.
  */
 function hook_og_selective_info() {
   return array(
     'private' => t('Private'),
-  ); 
+  );
 }
 
 /**
  * Insert state and data, that will be saved with the group post.
- * 
+ *
  * @param $alter
  *   Array keyed by "state" and "data" passed by reference.
  *   The data passed by reference.
@@ -116,7 +116,7 @@ function hook_og_field_insert(&$alter, $obj_type, $object, $field, $instance, $l
 
 /**
  * Update state and data, that will be saved with the group post.
- * 
+ *
  * @param $alter
  *   Array keyed by "state" and "data" passed by reference.
  *   The data passed by reference.
@@ -135,25 +135,25 @@ function hook_og_field_update (&$alter, $obj_type, $object, $field, $instance, $
 
 /**
  * Alter the groups an object is associated with.
- * 
- * User subscription for example is passed through here, allows modules to 
+ *
+ * User subscription for example is passed through here, allows modules to
  * change them. Also user unsubscription is using this function. To identify
  * the type of action, we get the $op argument.
- * 
+ *
  * @param $edit
- *   An array with the groups the object will be associated with, passed by 
+ *   An array with the groups the object will be associated with, passed by
  *   reference.
  * @param $account
  *   The user being subscribed.
   @param $op
- *   Optional; The operation that is being done (e.g. "subscribe user" or 
+ *   Optional; The operation that is being done (e.g. "subscribe user" or
  *   "unsubscribe content").
  */
 function hook_og_set_association_alter(&$gids, $account, $op = '') {
   if ($op == 'subscribe user') {
-    // 	Subscribe the user to another group.
+    //   Subscribe the user to another group.
     $gids[] = 1;
-  }  
+  }
 }
 
 
