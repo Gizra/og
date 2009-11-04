09390c6e4543608a764d813904984d9d6de5c7fd
Amitaibu <xxx@xxx.com>
2009-11-04 09:58:39 +0200

    Implement og_entity_get_label_info() hooks, so any fieldable entity label (e.g. node's title) can be show in the OG audience field.

--- og.api.php
+++ og.api.php
@@ -242,6 +242,31 @@ function hook_og_users_roles_revoke($nid, $uid, $rid) {
 
 }
 
+/**
+ * Define the table, ID and label columns of a fieldable entity.
+ *
+ * This is used so groups can appear in the OG audience field with their
+ * sanitized name.
+ */
+function hook_og_entity_get_label_info() {
+  return array(
+    'my_entity' => array(
+      'table' => 'foo',
+      'id' => 'bar',
+      'label' => 'baz',
+    ),
+  );
+}
+
+/**
+ * Alter get entity label definitions.
+ *
+ */
+function hook_og_entity_get_label_info_alter(&$data) {
+  if (!empty($data['my_entity'])) {
+    $data['my_entity']['table'] = 'new_foo';
+  }
+}
 
 /**
  * @} End of "addtogroup hooks".
