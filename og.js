eb93a818f7621153274a94f832db8361fee58a04
Amitai Burstein <amitai-git@gizra.com>
2010-01-24 14:44:56 +0000

    #679770 by gordon: Added Additional Vertical Tabs JS.

--- og.js
+++ og.js
@@ -16,3 +16,38 @@ Drupal.behaviors.og_content_type = function() {
     $('#edit-og-max-groups').attr('disabled','disabled');
   };
 }
+
+
+Drupal.verticalTabs = Drupal.verticalTabs || {};
+
+Drupal.verticalTabs.og_nodeapi = function() {
+  var values = [];
+  $('.vertical-tabs-og_nodeapi #edit-og-groups :selected')
+    .each(function (i, selected) {
+      values[i] = $(selected).text();
+    });
+
+  return values.join(', ');
+}
+
+Drupal.verticalTabs.og = function() {
+  var type = $('.vertical-tabs-og input[type=radio]:checked').val();
+  
+  switch (type) {
+    case 'group':
+      return Drupal.t('Group node');
+      break;
+      
+    case 'omitted':
+      return Drupal.t('May not be posted into a group.');
+      break;
+      
+    case 'group_post_standard':
+      return Drupal.t('Standard group post');
+      break;
+      
+    case 'group_post_wiki':
+      return Drupal.t('Wiki group post');
+      break;
+  }
+}
