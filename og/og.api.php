4648030ef5f9fa4df754cdaf3cdcc3c81f27d157
Amitaibu <amitai@gizra.com>
2010-04-20 00:43:41 +0300

    Document hook_og_default_permissions().

--- og/og.api.php
+++ og/og.api.php
@@ -25,15 +25,6 @@ function hook_og_permission() {
 }
 
 /**
- * Set the default permissions to be assigned to members, by their role.
- */
-function hook_og_default_permissions() {
-  return array(
-    'foo' => array(OG_AUTHENTICATED_ROLE),
-  );
-}
-
-/**
  * Define group context handlers.
  *
  * @return
@@ -80,6 +71,47 @@ function hook_og_context_handlers_alter(&$items) {
  */
 function hook_og_default_roles() {
   return array('super admin');
+}
+
+/**
+ * Alter the default roles.
+ *
+ * The anonymous and authenticated member roles are not alterable.
+ *
+ * @param $roles
+ *   Array with the default roles name.
+ */
+function hook_og_default_roles_alter(&$roles) {
+  // Remove a default role.
+  unset($roles['super admin']);
+}
+
+/**
+ * Set the default permissions to be assigned to members, by their role.
+ *
+ * Roles should be defined via hook_og_default_roles().
+ *
+ * @return
+ *   Array keyed with the permission name and the roles it applied to as the
+ *   value.
+ */
+function hook_og_default_permissions() {
+  return array(
+    'foo' => array(OG_AUTHENTICATED_ROLE),
+  );
+}
+
+/**
+ * Alter the default permissions.
+ *
+ * @param $perms
+ *   Array keyed with the permission name and the roles it applied to as the
+ *   value.
+ */
+function hook_og_default_permissions_alter(&$perms) {
+  // Add the permission to 'super admin' role, that should be defined
+  // via hook_og_default_roles().
+  $perms['foo'][] = 'super admin';
 }
 
 function hook_og_users_roles_grant($nid, $uid, $rid) {
