63b724e823e45653c76250b78e636b69e8aecd11
Amitaibu <amitai@gizra.com>
2010-11-03 22:11:38 +0200

    Update api files and create api.php file for og-context.

--- og/og.api.php
+++ og/og.api.php
@@ -34,45 +34,6 @@ function hook_og_permission_alter(&$perms) {
 
 }
 
-/**
- * Define group context handlers.
- *
- * @return
- *   Array keyed with the context handler name and an array of properties:
- *   - callback: The callback function that should return a an array of group
- *     IDs.
- *   - menu: TRUE indicates that the handler will try to find a context by the
- *     current menu item. Defaults to TRUE.
- *   - menu path: If "menu" property is TRUE, this property is required.
- *     An array of path the handler should be invoked. For example,
- *     if the user is viewing a node, the menu system is "node/%", and all
- *     group context handlers with this matching path, will be invoked.
- *   - priority: Optional; Indicate if the context result of this handler should
- *     be treated as a priority. A use case can be for example, the "session"
- *     context handler that returns the group context that is stored in the
- *     $_SESSION. By giving it a priority, we make sure that even if viewing
- *     different pages, the user will see the same group context.
- *     @see og_context_handler_session().
- */
-function hook_og_context_handlers() {
-  $items = array();
-
-  $items['foo'] = array(
-    'callback' => 'foo_context_handler_bar',
-    'menu path' => array('foo/%', 'foo/%/bar'),
-  );
-
-  return $items;
-}
-
-/**
- * Alter the group context handlers.
- */
-function hook_og_context_handlers_alter(&$items) {
-  // Add another menu path that should invoke this handler.
-  $items['foo']['menu path'][] = 'foo/%/baz';
-}
-
 
 /**
  * Set a default role that will be used as a global role.
