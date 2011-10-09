a9d771062aa059b5a6df202adbfb8342bafa0fed
c4rl <c4rl@235047.no-reply.drupal.org>
2011-10-08 23:26:08 -0700

    Issue #496468 by c4rl: Allow admin to specify number of groups per content type for Wiki posts too.

--- og.js
+++ og.js
@@ -3,7 +3,7 @@
 Drupal.behaviors.og_content_type = function() {
   // Disable the group limit textarea if the content type is not a standard group post
   $('input[name="og_content_type_usage"]').click(function(){
-    if (!$('#edit-og-content-type-usage-group-post-standard').attr('checked')) {
+    if (!$('#edit-og-content-type-usage-group-post-standard').attr('checked') && !$('#edit-og-content-type-usage-group-post-wiki').attr('checked')) {
       $('#edit-og-max-groups').attr('disabled','disabled');
     } else {
       $('#edit-og-max-groups').removeAttr('disabled');
@@ -11,7 +11,7 @@ Drupal.behaviors.og_content_type = function() {
   });
   
   // Initial check to see if content type is standard group post
-  if (!$('#edit-og-content-type-usage-group-post-standard').attr('checked')) {
+  if (!$('#edit-og-content-type-usage-group-post-standard').attr('checked') && !$('#edit-og-content-type-usage-group-post-wiki').attr('checked')) {
     $('#edit-og-max-groups').attr('disabled','disabled');
   };
 }
