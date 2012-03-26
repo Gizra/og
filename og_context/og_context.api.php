907650632b6e5cecf232e7c0a92c458709021293
Amitaibu <amitai-git@gizra.com>
2012-03-26 03:35:09 +0200

    Minor comment fix.

--- og_context/og_context.api.php
+++ og_context/og_context.api.php
@@ -13,13 +13,13 @@
 
 /**
  * Add context negotiation info.
- * 
+ *
  * Define context "handlers".
  * - name: The human readable name of the context handler.
- * - Description: The desciption of the context handler.
+ * - Description: The description of the context handler.
  * - callback: The callback function that will evaluate and return the group IDs
  *   that it finds.
- * - menu path: Optional; The menu path as retrieved from menu_get_item() that 
+ * - menu path: Optional; The menu path as retrieved from menu_get_item() that
  *   is required for the context handler to be invoked.
  */
 function hook_og_context_negotiation_info() {
