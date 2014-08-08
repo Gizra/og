42e255e671d57a8aa0b8071d402df69b7e86a4e8
Roy Segall <roy.segall@gizra.com>
2014-08-09 01:19:21 +0300

    Mapping config entity.

--- src/Entity/OgRole.php
+++ src/Entity/OgRole.php
@@ -1,15 +1,20 @@
 <?php
 
 /**
- * Contain the OG role entity definition. This will be a config entity.
+ * Contain the OG role entity definition. This will be a content entity.
  */
-
 namespace Drupal\og\Entity;
 
-use Drupal\Core\Config\Entity\ConfigEntityBase;
-use Drupal\Core\Config\Entity\ConfigEntityInterface;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
 
 /**
  */
-class OgRole extends ConfigEntityBase implements ConfigEntityInterface {
+class OgRole extends ContentEntityBase {
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    // TODO: Implement baseFieldDefinitions() method.
+  }
 }
