0104264af7274f6ad11f8dcc69342f24cdec6627
Roy Segall <roy.segall@gizra.com>
2014-08-09 22:35:31 +0300

    Convert role permission into a content entity.

--- src/Entity/OgRolePermission.php
+++ src/Entity/OgRolePermission.php
@@ -1,16 +1,62 @@
 <?php
 
 /**
- * Contain the OG role permission entity definition. This will be a config
+ * Contain the OG role permission entity definition. This will be a content
  * entity.
  */
 
 namespace Drupal\og\Entity;
 
-use Drupal\Core\Config\Entity\ConfigEntityBase;
-use Drupal\Core\Config\Entity\ConfigEntityInterface;
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\FieldDefinition;
 
 /**
+ * OG mange permission for users per group as an autonomic authority on your
+ * Drupal's site. Each module that interact with OG, and OG as well, defines a
+ * permission, like the core's permission system. OG roles can get permission
+ * for each group type and for specific groups.
+ *
+ * @ContentEntityType(
+ *   id = "og_role_permission",
+ *   label = @Translation(OG role permission"),
+ *   module = "og",
+ *   base_table = "og_membership",
+ *   entity_keys = {
+ *     "id" = "id",
+ *     "bundle" = "type",
+ *   },
+ *   bundle_keys = {
+ *     "bundle" = "type"
+ *   }
+ * )
  */
-class OgRolePermission extends ConfigEntityBase implements ConfigEntityInterface {
+class OgRolePermission extends ContentEntityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = array();
+
+    $fields['id'] = FieldDefinition::create('integer')
+      ->setLabel(t('ID'))
+      ->setDescription(t('The identifier of the row.'))
+      ->setReadOnly(TRUE);
+
+    $fields['rid'] = FieldDefinition::create('entity_reference')
+      ->setLabel(t('Role ID'))
+      ->setDescription(t('The role object.'))
+      ->setSetting('target_type', 'og_role');
+
+    $fields['permission'] = FieldDefinition::create('string')
+      ->setLabel(t('Permission'))
+      ->setDescription(t('The permission it self'));
+
+    $fields['module'] = FieldDefinition::create('string')
+      ->setLabel(t('Module'))
+      ->setDescription(t('The module defining the permission'));
+
+    return $fields;
+  }
 }
