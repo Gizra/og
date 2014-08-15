4525cf7286b5b059ebb2f39573a4922dbb11d4e0
Roy Segall <roy.segall@gizra.com>
2014-08-15 22:44:59 +0300

    Declare the routing for the OG complex callback.

--- src/Controller/OG.php
+++ src/Controller/OG.php
@@ -4,6 +4,10 @@ namespace Drupal\og\Controller;
 
 use Drupal\entity\Entity\EntityFormDisplay;
 use Drupal\og\OgFieldBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Drupal\Component\Utility\Tags;
 
 class OG {
 
@@ -111,5 +115,60 @@ class OG {
 
     // Let other OG modules know we invalidate cache.
     \Drupal::moduleHandler()->invokeAll('og_invalidate_cache', $gids);
+  }
+
+  /**
+   * Autocomplete the label of an entity.
+   *
+   * @param Request $request
+   *   The request object that contains the typed tags.
+   * @param string $type
+   *   The widget type (i.e. 'single' or 'tags').
+   * @param string $field_name
+   *   The name of the entity reference field.
+   * @param string $entity_type
+   *   The entity type.
+   * @param string $bundle_name
+   *   The bundle name.
+   * @param string $entity_id
+   *   (optional) The entity ID the entity reference field is attached to.
+   *   Defaults to ''.
+   *
+   * @throws \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException
+   *   Throws access denied when either the field or field instance does not
+   *   exists or the user does not have access to edit the field.
+   *
+   * @return \Symfony\Component\HttpFoundation\JsonResponse
+   *   The matched labels as json.
+   */
+  public static function handleAutocomplete(Request $request, $type, $field_name, $entity_type, $bundle_name, $entity_id) {
+    $definitions = \Drupal::entityManager()->getFieldDefinitions($entity_type, $bundle_name);
+
+    if (!isset($definitions[$field_name])) {
+      throw new AccessDeniedHttpException();
+    }
+
+    $field_definition = $definitions[$field_name];
+    $access_control_handler = \Drupal::entityManager()->getAccessControlHandler($entity_type);
+    if ($field_definition->getType() != 'entity_reference' || !$access_control_handler->fieldAccess('edit', $field_definition)) {
+      throw new AccessDeniedHttpException();
+    }
+
+    // Get the typed string, if exists from the URL.
+    $items_typed = $request->query->get('q');
+    $items_typed = Tags::explode($items_typed);
+    $last_item = drupal_strtolower(array_pop($items_typed));
+
+    $prefix = '';
+    // The user entered a comma-separated list of entity labels, so we generate
+    // a prefix.
+    if ($type == 'tags' && !empty($last_item)) {
+      $prefix = count($items_typed) ? Tags::implode($items_typed) . ', ' : '';
+    }
+
+    //$matches = $this->entityReferenceAutocomplete->getMatches($field_definition, $entity_type, $bundle_name, $entity_id, $prefix, $last_item);
+    $matches[] = array('value' => $prefix . 2, 'label' => 'a');
+
+    return new JsonResponse($matches);
   }
 }
--- src/Plugin/Field/FieldWidget/OgComplex.php
+++ src/Plugin/Field/FieldWidget/OgComplex.php
@@ -2,14 +2,14 @@
 
 /**
  * @file
- * Contains \Drupal\entity_reference\Plugin\Field\FieldWidget\AutocompleteWidget.
+ * Contains \Drupal\og\Plugin\Field\FieldWidget\OgComplex.
  */
 
 namespace Drupal\og\Plugin\Field\FieldWidget;
 
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\entity_reference\Plugin\Field\FieldWidget\AutocompleteWidgetBase;
+use Drupal\entity_reference\Plugin\Field\FieldWidget\AutocompleteWidget;
 
 /**
  * Plugin implementation of the 'entity_reference autocomplete' widget.
@@ -23,73 +23,17 @@ use Drupal\entity_reference\Plugin\Field\FieldWidget\AutocompleteWidgetBase;
  *   }
  * )
  */
-class OgComplex extends AutocompleteWidgetBase {
-
-  protected $usesOptions = TRUE;
+class OgComplex extends AutocompleteWidget {
 
   /**
-   * {@inheritdoc}
+   * The OG complex widget have a special logic on order to return the groups
+   * that user can reference to.
    */
-  public static function defaultSettings() {
-    return array(
-      'match_operator' => 'CONTAINS',
-      'size' => '60',
-      'autocomplete_type' => 'tags',
-      'placeholder' => '',
-    ) + parent::defaultSettings();
-  }
+  public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
+    $return = parent::formElement($items, $delta, $element, $form, $form_state);
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function getEntityIds(FieldItemListInterface $items, $delta) {
-    // The autocomplete widget outputs one entity label per form element.
-    if (isset($items[$delta])) {
-      return array($items[$delta]->target_id);
-    }
+    $return['target_id']['#autocomplete_route_name'] = 'og.entity_reference.autocomplete';
 
-    return array();
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function elementValidate($element, FormStateInterface $form_state, $form) {
-    $auto_create = $this->getSelectionHandlerSetting('auto_create');
-
-    // If a value was entered into the autocomplete.
-    $value = NULL;
-    if (!empty($element['#value'])) {
-      // Take "label (entity id)', match the id from parenthesis.
-      // @todo: Lookup the entity type's ID data type and use it here.
-      // https://drupal.org/node/2107249
-      if ($this->isContentReferenced() && preg_match("/.+\((\d+)\)/", $element['#value'], $matches)) {
-        $value = $matches[1];
-      }
-      elseif (preg_match("/.+\(([\w.]+)\)/", $element['#value'], $matches)) {
-        $value = $matches[1];
-      }
-      if (!$value) {
-        // Try to get a match from the input string when the user didn't use the
-        // autocomplete but filled in a value manually.
-        $handler = \Drupal::service('plugin.manager.entity_reference.selection')->getSelectionHandler($this->fieldDefinition);
-        $value = $handler->validateAutocompleteInput($element['#value'], $element, $form_state, $form, !$auto_create);
-      }
-
-      if (!$value && $auto_create && (count($this->getSelectionHandlerSetting('target_bundles')) == 1)) {
-        // Auto-create item. See
-        // \Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem::presave().
-        $value = array(
-          'target_id' => NULL,
-          'entity' => $this->createNewEntity($element['#value'], $element['#autocreate_uid']),
-          // Keep the weight property.
-          '_weight' => $element['#weight'],
-        );
-        // Change the element['#parents'], so in form_set_value() we
-        // populate the correct key.
-        array_pop($element['#parents']);
-      }
-    }
-    form_set_value($element, $value, $form_state);
+    return $return;
   }
 }
