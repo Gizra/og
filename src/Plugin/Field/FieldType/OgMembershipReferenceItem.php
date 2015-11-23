e2a9bd4e746446331ee9a175dbcda64d2e990ca5
Damian Lee <damian@tag1consulting.com>
2015-11-23 16:58:59 +0000

    Port the access_override setting

--- src/Plugin/Field/FieldType/OgMembershipReferenceItem.php
+++ src/Plugin/Field/FieldType/OgMembershipReferenceItem.php
@@ -9,7 +9,7 @@ namespace Drupal\og\Plugin\Field\FieldType;
 
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\Field\Plugin\Field\FieldType\EntityReferenceItem;
-use Drupal\og\Controller\OG;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
  * Class OgMembershipReferenceItem.
@@ -27,6 +27,33 @@ use Drupal\og\Controller\OG;
  * )
  */
 class OgMembershipReferenceItem extends EntityReferenceItem {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function defaultFieldSettings() {
+    $settings = parent::defaultFieldSettings();
+    $settings['access_override'] = FALSE;
+
+    return $settings;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function fieldSettingsForm(array $form, FormStateInterface $form_state) {
+    $form = parent::fieldSettingsForm($form, $form_state);
+
+    // Field access settings.
+    $form['access_override'] = [
+      '#title' => $this->t('Allow entity access to control field access'),
+      '#description' => $this->t('By default, the <em>administer group</em> permission is required to directly edit this field. Selecting this option will allow access to anybody with access to edit the entity.'),
+      '#type' => 'checkbox',
+      '#default_value' => $this->getSetting('access_override'),
+    ];
+
+    return $form;
+  }
 
   /**
    * {@inheritdoc}
