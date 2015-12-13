fc54c2380dbf88cbcc1b93a7ad0e5e2717a5cd92
Roy Segall <roy.segall@gizra.com>
2015-12-13 12:50:08 +0200

    Extend core's role class.

--- src/Entity/OgRole.php
+++ src/Entity/OgRole.php
@@ -6,8 +6,7 @@
  */
 namespace Drupal\og\Entity;
 
-use Drupal\Core\Config\Entity\ConfigEntityBase;
-use Drupal\Core\Entity\EntityStorageInterface;
+use Drupal\user\Entity\Role;
 
 /**
  * Defines the OG user role entity class.
@@ -38,64 +37,28 @@ use Drupal\Core\Entity\EntityStorageInterface;
  *   }
  * )
  */
-class OgRole extends ConfigEntityBase implements OgRoleInterface {
-
-  /**
-   * The machine name of this role.
-   *
-   * @var string
-   */
-  protected $id;
-
-  /**
-   * The human-readable label of this role.
-   *
-   * @var string
-   */
-  protected $label;
-
-  /**
-   * The weight of this role in administrative listings.
-   *
-   * @var int
-   */
-  protected $weight;
+class OgRole extends Role implements OgRoleInterface {
 
   /**
    * @var integer
    *
    * The group ID.
    */
-  protected $groupID;
+  protected $group_id;
 
   /**
    * The entity type ID of the group.
    *
    * @var string
    */
-  protected $groupType;
+  protected $group_type;
 
   /**
    * The bundle ID of the group.
    *
    * @var string
    */
-  protected $groupBundle;
-
-
-  /**
-   * The permissions belonging to this role.
-   *
-   * @var array
-   */
-  protected $permissions = array();
-
-  /**
-   * An indicator whether the role has all permissions.
-   *
-   * @var bool
-   */
-  protected $is_admin;
+  protected $group_bundle;
 
   /**
    * @return string
@@ -128,7 +91,7 @@ class OgRole extends ConfigEntityBase implements OgRoleInterface {
    * @return OgRole
    */
   public function setGroupID($groupID) {
-    $this->groupID = $groupID;
+    $this->group_id = $groupID;
     $this->set('group_id', $groupID);
     return $this;
   }
@@ -146,7 +109,7 @@ class OgRole extends ConfigEntityBase implements OgRoleInterface {
    * @return OgRole
    */
   public function setGroupType($groupType) {
-    $this->groupType = $groupType;
+    $this->group_type = $groupType;
     $this->set('group_type', $groupType);
     return $this;
   }
@@ -164,108 +127,9 @@ class OgRole extends ConfigEntityBase implements OgRoleInterface {
    * @return OgRole
    */
   public function setGroupBundle($groupBundle) {
-    $this->groupBundle = $groupBundle;
+    $this->group_bundle = $groupBundle;
     $this->set('group_bundle', $groupBundle);
     return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getPermissions() {
-    if ($this->isAdmin()) {
-      return [];
-    }
-    return $this->permissions;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function getWeight() {
-    return $this->get('weight');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setWeight($weight) {
-    $this->set('weight', $weight);
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function hasPermission($permission) {
-    if ($this->isAdmin()) {
-      return TRUE;
-    }
-    return in_array($permission, $this->permissions);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function grantPermission($permission) {
-    if ($this->isAdmin()) {
-      return $this;
-    }
-    if (!$this->hasPermission($permission)) {
-      $this->permissions[] = $permission;
-    }
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function revokePermission($permission) {
-    if ($this->isAdmin()) {
-      return $this;
-    }
-    $this->permissions = array_diff($this->permissions, array($permission));
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function isAdmin() {
-    return (bool) $this->is_admin;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function setIsAdmin($is_admin) {
-    $this->is_admin = $is_admin;
-    return $this;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function postLoad(EntityStorageInterface $storage, array &$entities) {
-    parent::postLoad($storage, $entities);
-    // Sort the queried roles by their weight.
-    // See \Drupal\Core\Config\Entity\ConfigEntityBase::sort().
-    uasort($entities, 'static::sort');
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function preSave(EntityStorageInterface $storage) {
-    parent::preSave($storage);
-
-    if (!isset($this->weight) && ($roles = $storage->loadMultiple())) {
-      // Set a role weight to make this new role last.
-      $max = array_reduce($roles, function($max, $role) {
-        return $max > $role->weight ? $max : $role->weight;
-      });
-      $this->weight = $max + 1;
-    }
   }
 
 }
