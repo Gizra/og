7313c904be794018ace7464c81d5d06bfd4ef252
Amitai Burstein <amitai@gizra.com>
2015-12-13 19:57:18 +0200

    Split IF to multiple IFs for better readability

--- src/OgGroupAudienceHelper.php
+++ src/OgGroupAudienceHelper.php
@@ -61,7 +61,7 @@ class OgGroupAudienceHelper {
    * Returns the first group audience field that matches the given entity.
    *
    * @param \Drupal\Core\Entity\ContentEntityInterface $entity
-   *   The content entity to find a matching group audience field for.
+   *   The group content to find a matching group audience field for.
    * @param string $group_type
    *   The group type that should be referenced by the group audience field.
    * @param string $group_bundle
@@ -79,27 +79,29 @@ class OgGroupAudienceHelper {
 
     // Bail out if there are no group audience fields.
     if (!$fields) {
-      // @todo Throw an exception or return an empty string instead of returning
-      //   NULL?
       return NULL;
     }
 
     foreach ($fields as $field_name => $field) {
       $handler_settings = $field->getSetting('handler_settings');
 
-      if (
-        // Skip if the group type doesn't match.
-        $field->getSetting('target_type') !== $group_type ||
+      if ($field->getSetting('target_type') !== $group_type) {
+        // Group type doesn't match.
+        continue;
+      }
 
-        // Skip if the bundle doesn't match.
-        (!empty($handler_settings['target_bundles']) && !in_array($group_bundle, $handler_settings['target_bundles'])) ||
+      if (!empty($handler_settings['target_bundles']) && !in_array($group_bundle, $handler_settings['target_bundles'])) {
+        // Bundle doesn't match.
+        continue;
+      }
 
-        // Skip if the field reached maximum.
-        !static::checkFieldCardinality($entity, $field_name) ||
+      if (!static::checkFieldCardinality($entity, $field_name)) {
+        // The field cardinality has reached its maximum
+        continue;
+      }
 
-        // Skip if the user doesn't have access to the field.
-        ($check_access && !$entity->$field_name->access('view'))
-      ) {
+      if ($check_access && !$entity->$field_name->access('view')) {
+        // The user doesn't have access to the field.
         continue;
       }
 
