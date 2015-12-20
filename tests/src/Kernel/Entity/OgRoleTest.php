8f4ea9b9be82876dec5dfba58bc9147dc1fba6f0
Roy Segall <roy.segall@gizra.com>
2015-12-20 15:28:30 +0200

    Add test.

--- tests/src/Kernel/Entity/OgRoleTest.php
+++ tests/src/Kernel/Entity/OgRoleTest.php
@@ -7,6 +7,7 @@
 
 namespace Drupal\Tests\og\Kernel\Entity;
 
+use Drupal\Core\Entity\EntityStorageException;
 use Drupal\KernelTests\KernelTestBase;
 use Drupal\og\Entity\OgRole;
 
@@ -46,6 +47,18 @@ class OgRoleTest extends KernelTestBase {
     // Checking creation of the role.
     $this->assertEquals($og_role->getPermissions(), ['administer group']);
 
+    // Try to create the same role again.
+    try {
+      $og_role = OgRole::create();
+      $og_role
+        ->setId('content_editor')
+        ->setLabel('Content editor')
+        ->grantPermission('administer group')
+        ->save();
+    } catch (EntityStorageException $e) {
+      $this->assertTrue(TRUE, "OG role with the same ID can not be saved.");
+    }
+
     // Create a role assigned to a group type.
     $og_role = OgRole::create();
     $og_role
@@ -56,6 +69,20 @@ class OgRoleTest extends KernelTestBase {
       ->save();
 
     $this->assertEquals('entity_test-group-content_editor', $og_role->id());
+
+    // Try to create the same role again.
+    try {
+      $og_role = OgRole::create();
+      $og_role
+        ->setId('content_editor')
+        ->setLabel('Content editor')
+        ->setGroupType('entity_test')
+        ->setGroupBundle('group')
+        ->save();
+
+    } catch (EntityStorageException $e) {
+      $this->assertTrue(TRUE, "OG role with the same ID on the same group can not be saved.");
+    }
   }
 
 }
