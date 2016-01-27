9a99e45e8df0a461b4b8c2ad5d011bdba9ca2713
Pieter Frenssen <pieter@frenssen.be>
2016-01-27 10:57:15 +0200

    Fix the 'Other Groups' field.

--- src/Plugin/Field/FieldWidget/OgComplex.php
+++ src/Plugin/Field/FieldWidget/OgComplex.php
@@ -306,10 +306,14 @@ class OgComplex extends EntityReferenceAutocompleteWidget {
    * {@inheritdoc}
    */
   public function massageFormValues(array $values, array $form, FormStateInterface $form_state) {
-    $parent_values = $values;
+    // Remove empty values. The form fields may be empty.
+    $values = array_filter($values, function ($item) {
+      return !empty($item['target_id']);
+    });
 
     // Get the groups from the other groups widget.
-    foreach ($form['other_groups'] as $key => $value) {
+    // @todo Don't hardcode 'og_group_ref' - there can be multiple fields.
+    foreach ($form['og_group_ref']['other_groups'] as $key => $value) {
       if (!is_int($key)) {
         continue;
       }
@@ -318,18 +322,16 @@ class OgComplex extends EntityReferenceAutocompleteWidget {
       // be captured in it's own group, with the key 'id'.
       preg_match("|.+\((?<id>[\w.]+)\)|", $value['target_id']['#value'], $matches);
 
-      if (empty($matches['id'])) {
-        continue;
+      if (!empty($matches['id'])) {
+        $values[] = [
+          'target_id' => $matches['id'],
+          '_weight' => $value['_weight']['#value'],
+          '_original_delta' => $value['_weight']['#delta'],
+        ];
       }
-
-      $parent_values[] = [
-        'target_id' => $matches['id'],
-        '_weight' => $value['_weight']['#value'],
-        '_original_delta' => $value['_weight']['#delta'],
-      ];
     }
 
-    return $parent_values;
+    return $values;
   }
 
   /**
