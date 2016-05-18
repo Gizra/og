5c97c8c7a4efd72f421e882e2ec0909f4719aa29
Amitaibu <amitai@gizra.com>
2016-05-18 12:22:47 +0300

    fix missing OG type

--- tests/src/Kernel/Access/OgEntityAccessTest.php
+++ tests/src/Kernel/Access/OgEntityAccessTest.php
@@ -10,6 +10,7 @@ use Drupal\og\Entity\OgMembership;
 use Drupal\og\Entity\OgRole;
 use Drupal\og\Og;
 use Drupal\og\OgGroupAudienceHelper;
+use Drupal\og\OgMembershipInterface;
 use Drupal\user\Entity\Role;
 use Drupal\user\Entity\User;
 /**
@@ -69,16 +70,20 @@ class OgEntityAccessTest extends KernelTestBase {
     $this->installEntitySchema('user');
     $this->installEntitySchema('node');
     $this->installSchema('system', 'sequences');
+
     // Creating a user so we won't test with uid 1.
     User::create(['name' => $this->randomString()])->save();
+
     // Creating a user and attach a group audience field.
     $this->user = User::create(['id' => 2, 'name' => $this->randomString()]);
     $this->user->save();
     Og::createField(OgGroupAudienceHelper::DEFAULT_FIELD, 'user', 'user');
+
     /** @var Role $role */
     $role = Role::load(Role::AUTHENTICATED_ID);
     $role->grantPermission('access content');
     $role->save();
+
     // Creating groups.
     $this->groupBundle = $this->randomMachineName();
     NodeType::create([
@@ -91,13 +96,18 @@ class OgEntityAccessTest extends KernelTestBase {
       'type' => $this->groupBundle,
     ]);
     $this->nodeGroup->save();
+
     Og::groupManager()->addGroup('node', $this->groupBundle);
-    $membership = OgMembership::create();
+
+
+    /** @var OgMembership $membership */
+    $membership = OgMembership::create(['type' => OgMembershipInterface::TYPE_DEFAULT]);
     $membership
       ->setUser($this->user->id())
       ->setEntityId($this->nodeGroup->id())
-      ->setEntityType($this->nodeGroup->getEntityTypeId())
+      ->setGroupEntityType($this->nodeGroup->getEntityTypeId())
       ->save();
+
     $this->ogRole = OgRole::create();
     $this->ogRole
       ->setId('content_editor')
@@ -105,18 +115,21 @@ class OgEntityAccessTest extends KernelTestBase {
       ->setGroupType('node')
       ->setGroupBundle($this->groupBundle)
       ->save();
+
     // Create two node group content.
     $this->nodeGroupBundle = $this->randomMachineName();
     NodeType::create([
       'type' => $this->nodeGroupBundle,
       'name' => $this->randomString(),
     ])->save();
+
     Og::createField(OgGroupAudienceHelper::DEFAULT_FIELD, 'node', $this->nodeGroupBundle);
     $this->nodeGroupContent[] = Node::create([
       'title' => $this->randomString(),
       'uid' => $this->user->id(),
       'type' => $this->nodeGroupBundle,
     ]);
+
     $this->nodeGroupContent[0]->set(OgGroupAudienceHelper::DEFAULT_FIELD, [$this->nodeGroup->id()]);
     $this->nodeGroupContent[0]->save();
     $this->nodeGroupContent[] = Node::create([
@@ -124,6 +137,7 @@ class OgEntityAccessTest extends KernelTestBase {
       'uid' => 3,
       'type' => $this->nodeGroupBundle,
     ]);
+
     $this->nodeGroupContent[1]->set(OgGroupAudienceHelper::DEFAULT_FIELD, $this->nodeGroup->id());
     $this->nodeGroupContent[1]->save();
   }
@@ -135,9 +149,5 @@ class OgEntityAccessTest extends KernelTestBase {
     $this->ogRole->grantPermission('edit own ' . $this->nodeGroupContent[1]->bundle() . ' content')->save();
     $this->assertTrue($this->nodeGroupContent[0]->access('edit', $this->user));
   }
-  /**
-   * Testing group's permission inside non-node groups.
-   */
-  public function testEntityTestGroupPermission() {
-  }
+
 }
