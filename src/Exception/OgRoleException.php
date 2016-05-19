a39608c1bfacd07c7ee3a23d498a9e83fe983638
Pieter Frenssen <pieter@frenssen.be>
2016-05-19 09:54:42 +0300

    Revert "Throw an exception when a role already exists when it is being created."
    This reverts commit 22ac772a6f6acb0e4febe80f950a7abb8de5fce0.

--- src/Exception/OgRoleException.php
+++ src/Exception/OgRoleException.php
@@ -3,7 +3,7 @@
 namespace Drupal\og\Exception;
 
 /**
- * Exception thrown when an invalid action is performed on an OG role.
+ * Exception thrown when an invalid action is performed on immutable properties.
  */
 class OgRoleException extends \LogicException {
 }
