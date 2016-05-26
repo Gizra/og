4e30246c8d445cafe7a3e979c1ee8fcabe5101c8
Pieter Frenssen <pieter@frenssen.be>
2016-05-26 09:42:21 +0300

    Provide test cases and skeleton tests for invalid roles.

--- tests/src/Unit/DefaultRoleEventTest.php
+++ tests/src/Unit/DefaultRoleEventTest.php
@@ -155,6 +155,61 @@ class DefaultRoleEventTest extends UnitTestCase {
   }
 
   /**
+   * @param array $invalid_roles
+   *   An array of invalid test default roles.
+   *
+   * @covers ::addRole
+   *
+   * @dataProvider invalidDefaultRoleProvider
+   */
+  public function testAddInvalidRole($invalid_roles) {
+  }
+
+  /**
+   * @param array $invalid_roles
+   *   An array of invalid test default roles.
+   *
+   * @covers ::addRoles
+   *
+   * @dataProvider invalidDefaultRoleProvider
+   */
+  public function testAddInvalidRoles($invalid_roles) {
+  }
+
+  /**
+   * @param array $invalid_roles
+   *   An array of invalid test default roles.
+   *
+   * @covers ::setRole
+   *
+   * @dataProvider invalidDefaultRoleProvider
+   */
+  public function testSetInvalidRole($invalid_roles) {
+  }
+
+  /**
+   * @param array $invalid_roles
+   *   An array of invalid test default roles.
+   *
+   * @covers ::setRoles
+   *
+   * @dataProvider invalidDefaultRoleProvider
+   */
+  public function testSetInvalidRoles($invalid_roles) {
+  }
+
+  /**
+   * @param array $invalid_roles
+   *   An array of invalid test default roles.
+   *
+   * @covers ::offsetSet
+   *
+   * @dataProvider invalidDefaultRoleProvider
+   */
+  public function testInvalidOffsetSet($invalid_roles) {
+  }
+
+  /**
    * Provides test data to test default roles.
    *
    * @return array
@@ -163,8 +218,75 @@ class DefaultRoleEventTest extends UnitTestCase {
    */
   public function defaultRoleProvider() {
     return [
+      // Test adding a single administrator role with only a label.
       [
         OgRoleInterface::ADMINISTRATOR => ['label' => $this->t('Administrator')],
+      ],
+      // Test adding a single administrator role with a label and role type.
+      [
+        OgRoleInterface::ADMINISTRATOR => [
+          'label' => $this->t('Administrator'),
+          'role_type' => OgRoleInterface::ROLE_TYPE_REQUIRED,
+        ],
+      ],
+      // Test adding multiple roles.
+      [
+        OgRoleInterface::ADMINISTRATOR => [
+          'label' => $this->t('Administrator'),
+          'role_type' => OgRoleInterface::ROLE_TYPE_REQUIRED,
+        ],
+        'moderator' => [
+          'label' => $this->t('Moderator'),
+          'role_type' => OgRoleInterface::ROLE_TYPE_STANDARD,
+        ],
+        'contributor' => [
+          'label' => $this->t('Contributor'),
+        ],
+      ],
+    ];
+  }
+
+  /**
+   * Provides invalid test data to test default roles.
+   *
+   * @return array
+   *   An array of test data arrays, each test data array containing an array of
+   *   invalid test default roles, keyed by default role name.
+   */
+  public function invalidDefaultRoleProvider() {
+    return [
+      // A role with a missing name.
+      [
+        '' => ['label' => $this->t('Administrator')],
+      ],
+      // A role without a label.
+      [
+        OgRoleInterface::ADMINISTRATOR => [
+          'role_type' => OgRoleInterface::ROLE_TYPE_REQUIRED,
+        ],
+      ],
+      // A role with an invalid role type.
+      [
+        OgRoleInterface::ADMINISTRATOR => [
+          'label' => $this->t('Administrator'),
+          'role_type' => 'Some non-existing role type',
+        ],
+      ],
+      // An array of multiple correct roles, with one invalid role type sneaked
+      // in.
+      [
+        OgRoleInterface::ADMINISTRATOR => [
+          'label' => $this->t('Administrator'),
+          'role_type' => OgRoleInterface::ROLE_TYPE_REQUIRED,
+        ],
+        'moderator' => [
+          'label' => $this->t('Moderator'),
+          'role_type' => OgRoleInterface::ROLE_TYPE_STANDARD,
+        ],
+        'contributor' => [
+          'label' => $this->t('Contributor'),
+          'role_type' => 'Some non-existing role type',
+        ],
       ],
     ];
   }
