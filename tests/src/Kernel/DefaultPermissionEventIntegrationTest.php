b27fd2d95f51e58c9d3ab8b82423c2e84f7dfa2a
Pieter Frenssen <pieter@frenssen.be>
2016-05-26 16:24:45 +0300

    Test that the 'administrator' default role is correctly provided by the event subscriber.

---
+++ tests/src/Kernel/DefaultPermissionEventIntegrationTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Drupal\Tests\og\Kernel;
+
+use Drupal\Core\Entity\Entity;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\og\Event\DefaultRoleEvent;
+use Drupal\og\Event\DefaultRoleEventInterface;
+use Drupal\og\Og;
+use Drupal\og\OgRoleInterface;
+
+/**
+ * Tests the implementation of the DefaultRoleEvent in the 'og' module.
+ *
+ * @group og
+ */
+class DefaultRoleEventIntegrationTest extends KernelTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static $modules = ['entity_test', 'og', 'system', 'user'];
+
+  /**
+   * The Symfony event dispatcher.
+   *
+   * @var \Symfony\Component\EventDispatcher\EventDispatcherInterface
+   */
+  protected $eventDispatcher;
+
+  /**
+   * The group bundle ID of the test group.
+   *
+   * @var string
+   */
+  protected $groupBundleId;
+
+  /**
+   * The OG role storage handler.
+   *
+   * @var \Drupal\Core\Entity\EntityStorageInterface
+   */
+  protected $ogRoleStorage;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    parent::setUp();
+
+    $this->eventDispatcher = $this->container->get('event_dispatcher');
+    $this->ogRoleStorage = $this->container->get('entity_type.manager')->getStorage('og_role');
+
+    // Create a group entity type. Note that since we are using the EntityTest
+    // entity we don't actually need to create the group bundle. EntityTest does
+    // not have real bundles, it just pretends it does.
+    $this->groupBundleId = $this->randomMachineName();
+    Og::groupManager()->addGroup('entity_test', $this->groupBundleId);
+  }
+
+  /**
+   * Tests that OG correctly provides the group administrator default role.
+   */
+  public function testPermissionEventIntegration() {
+    // Query the event listener directly to see if the administrator role is
+    // present.
+    /** @var DefaultRoleEvent $event */
+    $event = $this->eventDispatcher->dispatch(DefaultRoleEventInterface::EVENT_NAME, new DefaultRoleEvent());
+    $expected_roles = [
+      OgRoleInterface::ADMINISTRATOR => [
+        'label' => 'Administrator',
+        'role_type' => OgRoleInterface::ROLE_TYPE_STANDARD,
+      ],
+    ];
+    $this->assertEquals($event->getRoles(), $expected_roles);
+
+    // Check that the per-group-type default roles are populated.
+    $expected_roles = [
+      OgRoleInterface::ANONYMOUS,
+      OgRoleInterface::AUTHENTICATED,
+      OgRoleInterface::ADMINISTRATOR,
+    ];
+    $actual_roles = $this->ogRoleStorage->loadByProperties([
+      'group_type' => 'entity_test',
+      'group_bundle' => $this->groupBundleId,
+    ]);
+
+    $this->assertEquals(count($expected_roles), count($actual_roles));
+
+    foreach ($expected_roles as $expected_role) {
+      // The role ID consists of the entity type, bundle and role name.
+      $expected_key = implode('-', [
+        'entity_test',
+        $this->groupBundleId,
+        $expected_role,
+      ]);
+      $this->assertArrayHasKey($expected_key, $actual_roles);
+    }
+  }
+
+}
