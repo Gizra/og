dd7aa5e08529b4f3bd663c809646c5a39d16a6da
Pieter Frenssen <pieter@frenssen.be>
2016-07-25 10:38:43 +0300

    Remove superfluous inline type hint.

--- tests/src/Kernel/Entity/OgMembershipTest.php
+++ tests/src/Kernel/Entity/OgMembershipTest.php
@@ -62,7 +62,6 @@ class OgMembershipTest extends KernelTestBase {
     $this->installEntitySchema('user');
     $this->installSchema('system', 'sequences');
 
-    /** @var \Drupal\Core\Entity\EntityTypeManagerInterface entityTypeManager */
     $this->entityTypeManager = $this->container->get('entity_type.manager');
 
     // Create a bundle and add as a group.
