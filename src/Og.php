5a586b252633b90e620b5895b84b49f5d5a5b61f
Amitai Burstein <amitai@gizra.com>
2016-07-29 21:07:23 +0300

    Add Og::getRole

--- src/Og.php
+++ src/Og.php
@@ -9,6 +9,7 @@ use Drupal\Core\Session\AccountInterface;
 use Drupal\field\Entity\FieldConfig;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\field\FieldStorageConfigInterface;
+use Drupal\og\Entity\OgRole;
 
 /**
  * A static helper class for OG.
@@ -576,6 +577,36 @@ class Og {
   public static function groupManager() {
     // @todo store static reference for this?
     return \Drupal::service('og.group.manager');
+  }
+
+  /**
+   * Get a role by the group's bundle and role name
+   *
+   * @param string $entity_type_id
+   *   The group entity type ID.
+   * @param string $bundle
+   *   The group bundle name.
+   * @param $role_name
+   *   The role name.
+   *
+   * @return \Drupal\og\OgRoleInterface|null
+   *   The OG role object, or NULL if a matching role was not found.
+   */
+  public static function getRole($entity_type_id, $bundle, $role_name) {
+    $result = \Drupal::entityQuery('og_role')
+      ->condition('group_type', $entity_type_id)
+      ->condition('group_bundle', $bundle)
+      ->condition('label', $role_name)
+      // We will have only one matching role.
+      ->range(0, 1)
+      ->execute();
+
+    if (!$result) {
+      return NULL;
+    }
+
+    $id = reset($result);
+    return OgRole::load($id);
   }
 
   /**
