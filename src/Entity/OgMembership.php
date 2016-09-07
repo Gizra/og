1841af64b7d0efc6a54536e2b8cc62d6a2de93d5
Amitai Burstein <amitai@gizra.com>
2016-09-07 20:45:48 +0300

    Improve exception message

--- src/Entity/OgMembership.php
+++ src/Entity/OgMembership.php
@@ -337,7 +337,7 @@ class OgMembership extends ContentEntityBase implements OgMembershipInterface {
       ->execute();
 
     if ($count) {
-      throw new \LogicException(sprintf('A membership exists for this group, of entity type %s with ID: %s', $entity_type_id, $this->getGroup()->id()));
+      throw new \LogicException(sprintf('An OG membership already exists for group of entity-type %s and ID: %s', $entity_type_id, $this->getGroup()->id()));
     }
 
     parent::preSave($storage);
