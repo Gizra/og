4f4eb9e71ad575a4279a402f2f9e1fd0384a3d9c
Amitaibu <amitai@gizra.com>
2016-09-07 11:39:37 +0300

    Add testNoMembership

--- tests/src/Unit/OgMembershipStateCacheContextTest.php
+++ tests/src/Unit/OgMembershipStateCacheContextTest.php
@@ -2,11 +2,13 @@
 
 namespace Drupal\Tests\og\Unit;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\og\Cache\Context\OgMembershipStateCacheContext;
 use Drupal\og\GroupTypeManager;
 use Drupal\og\MembershipManagerInterface;
+use Drupal\og\OgMembershipInterface;
 use Drupal\Tests\UnitTestCase;
 use Symfony\Component\Routing\Route;
 
@@ -77,12 +79,22 @@ class OgMembershipStateCacheContextTest extends UnitTestCase {
   protected $parameters;
 
   /**
+   * The group entity.
+   *
+   * @var \Drupal\Core\Entity\EntityInterface|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $group;
+
+  /**
    * {@inheritdoc}
    */
   public function setUp() {
-    $this->user = $this->prophesize(AccountInterface::class);;
-    $this->routeMatch = $this->prophesize(RouteMatchInterface::class);;
+    $this->user = $this->prophesize(AccountInterface::class);
+    $this->routeMatch = $this->prophesize(RouteMatchInterface::class);
+
+    $this->group = $this->prophesize(EntityInterface::class);
     $this->groupTypeManager = $this->prophesize(GroupTypeManager::class);
+
     $this->membershipManager = $this->prophesize(MembershipManagerInterface::class);
 
     $this->route = $this->prophesize(Route::class);
@@ -172,6 +184,48 @@ class OgMembershipStateCacheContextTest extends UnitTestCase {
 
     $result = $this->getContextResult();
     $this->assertEquals('none', $result);
+  }
+
+  /**
+   * Tests user with no membership.
+   *
+   * @covers ::getContext
+   */
+  public function testNoMembership() {
+    $this
+      ->routeMatch
+      ->getRouteObject()
+      ->willReturn($this->route->reveal());
+
+    $this
+      ->route
+      ->getOption('parameters')
+      ->willReturn($this->parameters);
+
+    $this
+      ->groupTypeManager
+      ->getAllGroupBundles()
+      ->willReturn($this->groupEntities);
+
+    $this
+      ->routeMatch
+      ->getParameter($this->entityTypeId)
+      ->willReturn($this->group->reveal());
+
+    $states = [
+      OgMembershipInterface::STATE_ACTIVE,
+      OgMembershipInterface::STATE_PENDING,
+      OgMembershipInterface::STATE_BLOCKED,
+    ];
+
+    $this
+      ->membershipManager
+      ->getMembership($this->group->reveal(), $this->user->reveal(), $states)
+      ->willReturn(FALSE);
+
+    $result = $this->getContextResult();
+    $this->assertEquals('none', $result);
+
   }
 
   /**
