885996deee4c50c94a5eaf880daa10ea22af9d0b
Pieter Frenssen <pieter@frenssen.be>
2016-09-16 19:17:31 +0200

    Update plugin name, this has been renamed to OgGroupResolver.

--- tests/modules/og_test/src/Plugin/GroupResolver/UserGroupReference.php
+++ tests/modules/og_test/src/Plugin/GroupResolver/UserGroupReference.php
@@ -12,7 +12,7 @@ use Drupal\og\OgContextBase;
 /**
  * Get the group from a field reference attached to the current user.
  *
- * @GroupResolver(
+ * @OgGroupResolver(
  *  id = "user_group_reference",
  *  label = "User group reference",
  *  description = @Translation("A dummy plugin to return the first group from a field attach to the user.")
