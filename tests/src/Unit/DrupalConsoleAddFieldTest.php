14acc731157a29e1e59801cfa3edb1522e06cd38
RoySegall <roy.segall@gizra.com>
2016-09-26 12:13:42 +0100

    Push more work.

--- tests/src/Unit/DrupalConsoleAddFieldTest.php
+++ tests/src/Unit/DrupalConsoleAddFieldTest.php
@@ -2,12 +2,16 @@
 
 namespace Drupal\Tests\og\Unit;
 
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\og\Command\OgAddFieldCommand;
 use Drupal\og\Helper\OgDrupalConsoleHelperTrait;
+use Drupal\og\OgFieldBase;
 use Drupal\Tests\UnitTestCase;
-use Prophecy\Argument;
 use Symfony\Component\Console\Tester\CommandTester;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Drupal\Core\Entity\EntityManager;
+use Drupal\og\OgFieldsPluginManager;
+use Drupal\Core\Entity\EntityTypeManager;
 
 /**
  * Tests create membership helper function.
@@ -20,13 +24,70 @@ class DrupalConsoleAddFieldTest extends UnitTestCase {
   use OgDrupalConsoleHelperTrait;
 
   /**
+   * The mocked OG field manager plugin.
+   *
+   * @var \Drupal\og\OgFieldsPluginManager|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $OgFieldManager;
+
+  /**
+   * The mocked OG field manager plugin.
+   *
+   * @var \Drupal\Core\Entity\EntityManager|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $entityManager;
+
+  /**
+   * The mocked OG field manager plugin.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManager|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The mocked OG field manager plugin.
+   *
+   * @var \Drupal\Core\Entity\EntityInterface|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $entity;
+
+  /**
+   * The mocked OG field manager plugin.
+   *
+   * @var \Drupal\og\OgFieldBase|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $OgFieldBase;
+
+  /**
    * {@inheritdoc}
    */
   public function setUp() {
     parent::setUp();
 
+    $this->OgFieldManager = $this->prophesize(OgFieldsPluginManager::class);
+    $this->OgFieldBase = $this->prophesize(OgFieldBase::class);
+    $this->entityManager = $this->prophesize(EntityManager::class);
+    $this->entityTypeManager = $this->prophesize(EntityTypeManager::class);
+    $this->entity = $this->prophesize(EntityInterface::class);
+
+    $this->OgFieldBase->setFieldName('og_audience')->willReturn($this->OgFieldBase);
+    $this->OgFieldBase->setBundle('article')->willReturn($this->OgFieldBase);
+    $this->OgFieldBase->setEntityType('node')->willReturn($this->OgFieldBase);
+
+    $this->entityManager->getStorage('field_storage_config')->willReturn($this->entity->reveal());
+
+    $this->OgFieldManager
+      ->getDefinition('og_audience')
+      ->willReturn(['foo' => 'bar']);
+
+    $this->OgFieldManager
+      ->createInstance('og_audience')
+      ->willReturn($this->OgFieldBase->reveal());
+
     $container = new ContainerBuilder();
-//    $container->set('entity.manager', $this->entityManager->reveal());
+    $container->set('plugin.manager.og.fields', $this->OgFieldManager->reveal());
+    $container->set('entity.manager', $this->entityManager->reveal());
+    $container->set('entity_type.manager', $this->entityTypeManager->reveal());
     \Drupal::setContainer($container);
   }
 
@@ -40,16 +101,16 @@ class DrupalConsoleAddFieldTest extends UnitTestCase {
     $commandTester = new CommandTester($command);
     $command->setHelperSet($this->getHelperSet());
 
-//    $code = $commandTester->execute(
-//      [
-//        '--field_id' => 'og_audience',
-//        '--entity_type' => 'node',
-//        '--bundle' => 'article',
-//        '--target_entity' => 'node',
-//      ]
-//    );
+    $code = $commandTester->execute(
+      [
+        '--field_id' => 'og_audience',
+        '--entity_type' => 'node',
+        '--bundle' => 'article',
+        '--target_entity' => 'node',
+      ]
+    );
 
-//    $this->assertEquals(0, $code);
+    $this->assertEquals(0, $code);
 
     $this->assertTrue(1 == 1);
   }
