cf30f3ad801ba9c0c2fcc088396f8cd20cafab1f
Amitai Burstein <amitai@gizra.com>
2016-10-19 00:23:08 +0300

    gst

--- tests/src/Functional/OgSelectionConfigurationFormTest.php
+++ tests/src/Functional/OgSelectionConfigurationFormTest.php
@@ -68,6 +68,11 @@ class OgSelectionConfigurationFormTest extends BrowserTestBase {
     $user = $this->drupalCreateUser(['administer content types', 'administer node fields']);
     $this->drupalLogin($user);
 
+    $this->drupalGet('admin/structure/types/manage/group_content');
+    $this->assertSession()->statusCodeEquals(200);
+
+    debug($this->getSession()->getPage()->getHtml());
+
     $this->drupalGet('admin/structure/types/manage/group_content/fields/node.group_content.og_audience');
     $this->assertSession()->statusCodeEquals(200);
 
