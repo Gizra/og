2a008aaa4bdb70078678672e9d32c6107a96f4f3
RoySegall <roy.segall@gizra.com>
2016-11-02 10:01:30 +0200

    Adding needed services to the group manager service.

--- src/Command/OgAddGroupCommand.php
+++ src/Command/OgAddGroupCommand.php
@@ -7,6 +7,9 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Command\Command;
 use Drupal\Console\Command\Shared\ContainerAwareCommandTrait;
 use Drupal\Console\Style\DrupalStyle;
+use Drupal\Core\Entity\EntityTypeRepositoryInterface;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\og\GroupTypeManager;
 
 /**
  * Class OgAddGroupCommand.
@@ -16,6 +19,45 @@ use Drupal\Console\Style\DrupalStyle;
 class OgAddGroupCommand extends Command {
 
   use ContainerAwareCommandTrait;
+
+  /**
+   * Bundle info service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeBundleInfoInterface
+   */
+  protected $bundleInfo;
+
+  /**
+   * Entity type repository service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeRepositoryInterface
+   */
+  protected $entityTypeRepository;
+
+  /**
+   * Group manager service.
+   *
+   * @var \Drupal\og\GroupTypeManager
+   */
+  protected $groupTypeManager;
+
+  /**
+   * Constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeBundleInfoInterface $bundle_info
+   *   The bundle info service.
+   * @param \Drupal\Core\Entity\EntityTypeRepositoryInterface $entity_type_repository
+   *   The type repository service.
+   * @param \Drupal\og\GroupTypeManager $group_type_manager
+   *   The OG plugin manager.
+   */
+  public function __construct(EntityTypeBundleInfoInterface $bundle_info, EntityTypeRepositoryInterface $entity_type_repository, GroupTypeManager $group_type_manager) {
+    parent::__construct();
+
+    $this->bundleInfo = $bundle_info;
+    $this->entityTypeRepository = $entity_type_repository;
+    $this->groupTypeManager = $group_type_manager;
+  }
 
   /**
    * {@inheritdoc}
