5897952670533e2cbfd2f71481d5bee9c2f976d0
Pieter Frenssen <pieter@frenssen.be>
2016-11-05 12:10:07 +0100

    Add a standard test case, a single group resolver returning a single group.

--- tests/src/Unit/OgContextTest.php
+++ tests/src/Unit/OgContextTest.php
@@ -5,6 +5,10 @@ namespace Drupal\Tests\og\Unit;
 use Drupal\Component\Plugin\PluginManagerInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Config\ImmutableConfig;
+use Drupal\Core\Entity\ContentEntityInterface;
+use Drupal\Core\Entity\TypedData\EntityDataDefinition;
+use Drupal\Core\TypedData\TypedDataInterface;
+use Drupal\Core\TypedData\TypedDataManagerInterface;
 use Drupal\Tests\UnitTestCase;
 use Drupal\og\ContextProvider\OgContext;
 use Drupal\og\OgGroupResolverInterface;
@@ -35,6 +39,34 @@ class OgContextTest extends UnitTestCase {
   protected $configFactory;
 
   /**
+   * A mocked typed data manager service.
+   *
+   * @var \Drupal\Core\TypedData\TypedDataManagerInterface|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $typedDataManager;
+
+  /**
+   * A mocked typed data definition.
+   *
+   * @var \Drupal\Core\Entity\TypedData\EntityDataDefinition|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $dataDefinition;
+
+  /**
+   * A mocked typed data object.
+   *
+   * @var \Drupal\Core\TypedData\TypedDataInterface|\Prophecy\Prophecy\ObjectProphecy
+   */
+  protected $typedData;
+
+  /**
+   * An array of mocked test entities.
+   *
+   * @var \Drupal\Core\Entity\ContentEntityInterface[]|\Prophecy\Prophecy\ObjectProphecy[]
+   */
+  protected $entities;
+
+  /**
    * {@inheritdoc}
    */
   public function setUp() {
@@ -42,12 +74,43 @@ class OgContextTest extends UnitTestCase {
 
     $this->pluginManager = $this->prophesize(PluginManagerInterface::class);
     $this->configFactory = $this->prophesize(ConfigFactoryInterface::class);
+    $this->typedDataManager = $this->prophesize(TypedDataManagerInterface::class);
+    $this->dataDefinition = $this->prophesize(EntityDataDefinition::class);
+    $this->typedData = $this->prophesize(TypedDataInterface::class);
+
+    // When a ContextProvider creates a new Context object and sets the context
+    // value on it, the Context object will use the typed data manager service
+    // to get a DataDefinition object, which is an abstracted representation of
+    // the data. Mock the method calls that are used during creation of this
+    // DataDefinition object. In the case of OgContext this will return an entity.
+    $this->dataDefinition->setLabel(Argument::any())->willReturn($this->dataDefinition);
+    $this->dataDefinition->setDescription(Argument::any())->willReturn($this->dataDefinition);
+    $this->dataDefinition->setRequired(Argument::any())->willReturn($this->dataDefinition);
+    $this->dataDefinition->getConstraints()->willReturn([]);
+    $this->dataDefinition->setConstraints(Argument::any())->willReturn($this->dataDefinition);
+
+    $this->typedDataManager->createDataDefinition('entity')
+      ->willReturn($this->dataDefinition->reveal());
 
     // Mock the string translation service on the container, this will cover
     // calls to $this->t().
     $container = new Container();
     $container->set('string_translation', $this->getStringTranslationStub());
+
+    // Put the mocked typed data manager service on the container. This is used
+    // to set the context value.
+    $container->set('typed_data_manager', $this->typedDataManager->reveal());
     \Drupal::setContainer($container);
+
+    // Create 2 mock entities each for node, block_content and entity_test
+    // entities.
+    foreach (['node', 'block_content', 'entity_test'] as $type) {
+      for ($i = 0; $i < 2; $i++) {
+        $id = "$type-$i";
+        $entity = $this->prophesize(ContentEntityInterface::class);
+        $this->entities[$id] = $entity->reveal();
+      }
+    }
   }
 
   /**
@@ -73,6 +136,13 @@ class OgContextTest extends UnitTestCase {
    * @dataProvider getRuntimeContextsProvider
    */
   public function testGetRuntimeContexts(array $unqualified_context_ids, array $group_resolvers, $expected_context) {
+    // Make the test entities available in the local scope so we can use it in
+    // anonymous functions.
+    $entities = $this->entities;
+
+    // Translate the ID of the expected context to the actual test entity.
+    $expected_context_entity = !empty($expected_context) ? $entities[$expected_context] : NULL;
+
     // Return the list of OgGroupResolver plugins that are supplied in the test
     // case. These are expected to be retrieved from config.
     $group_resolvers_config = $this->prophesize(ImmutableConfig::class);
@@ -87,16 +157,29 @@ class OgContextTest extends UnitTestCase {
       $plugin->isPropagationStopped()
         ->willReturn(!empty($group_resolver['stop_propagation']));
       $plugin->resolve(Argument::type(OgResolvedGroupCollectionInterface::class))
-        ->will(function ($args) use ($group_resolver) {
+        ->will(function ($args) use ($entities, $group_resolver) {
           /** @var \Drupal\og\OgResolvedGroupCollectionInterface $collection */
           $collection = $args[0];
           foreach ($group_resolver['candidates'] as $candidate) {
-            // @todo Pass EntityInterface objects to addGroup().
-            $collection->addGroup($candidate);
+            $collection->addGroup($entities[$candidate]);
           }
         });
       $this->pluginManager->createInstance($id)
         ->willReturn($plugin);
+    }
+
+    // It is expected that the correct resolved group will be set on the Context
+    // object.
+    if ($expected_context !== FALSE) {
+      $this->typedDataManager->create($this->dataDefinition, $entities[$expected_context])
+        ->shouldBeCalled()
+        ->willReturn($this->typedData->reveal());
+
+      // If the group is correctly set as on the Context object, then it is
+      // reasonable to expect that it will be returned as a typed data object
+      // that will give back the group when it is asked for it.
+      $this->typedData->getValue()
+        ->willReturn($expected_context_entity);
     }
 
     $og_context = new OgContext($this->pluginManager->reveal(), $this->configFactory->reveal());
@@ -109,7 +192,7 @@ class OgContextTest extends UnitTestCase {
       $this->assertEquals([], $result);
     }
     else {
-      $this->assertEquals($expected_context, $result['og']->getContextData());
+      $this->assertEquals($expected_context_entity, $result['og']->getContextData()->getValue());
       // @todo Test the cacheability metadata.
     }
 
@@ -141,10 +224,10 @@ class OgContextTest extends UnitTestCase {
         ['node', 'og'],
         // Simulate 1 group resolver that returns 1 result.
         [
-          'route_group' => ['candidates' => 'node-1'],
+          'route_group' => ['candidates' => ['node-0']],
         ],
         // The group of type 'node' was found.
-        'node-1',
+        'node-0',
       ]
     ];
   }
