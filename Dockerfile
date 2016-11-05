cef77214b47c2c11bff39c7362209d5ffd5bb031
Nir Galon <nirgn975@gmail.com>
2016-11-06 00:07:21 +0200

    Add local docker

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,16 @@
-FROM nirgn975/docker-drupal
+FROM drupal:7-apache
+ENV DEBIAN_FRONTEND noninteractive
+
+# Install MySQL and start.
+RUN apt-get update
+RUN apt-get install -y \
+	mysql-server \
+  mysql-client
+RUN apt-get clean
+
+# Install composer.
+RUN curl -sS https://getcomposer.org/installer | php
+RUN mv composer.phar /usr/local/bin/composer
 
 COPY ./start.sh /var/www/html/start.sh
 RUN chmod +x /var/www/html/start.sh
--- start.sh
+++ start.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-echo "[start] Start MySQL server"
+# State MySQL service.
 service mysql start
 
-echo "[start] Install Drupal"
+# Install Drush.
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+composer global require drush/drush:7.*
+
+# Install Drupal.
 drush si standard --account-name=admin --account-pass=admin --db-url=mysql://root@127.0.0.1/drupal -y
 
-echo "[start] Install the module"
+# Install the module.
 # ...
 
-echo "[start] Run the tests"
+# Run the tests.
 # ...
