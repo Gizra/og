f00eb117ebc9b340c5c3262784409460c0e05d05
Nir Galon <nir@galon.io>
2016-11-15 11:51:43 +0200

    Run the tests manually in docker

--- start.sh
+++ start.sh
@@ -18,5 +18,9 @@ cd sites/all/modules/
 git clone https://github.com/Gizra/og.git --branch $branch_name --depth 1
 drush en -y og
 
-# Run the tests.
-# ...
+# Run the tests
+drush en -y simpletest
+cd cd -
+php ./scripts/run-tests.sh --php $(which php) --concurrency 4 --verbose --color --url http://localhost "Organic groups","Organic groups access","Organic groups context","Organic groups field access","Organic groups UI" 2>&1 | tee /tmp/simpletest-result.txt
+egrep -i "([1-9]+ fail)|(Fatal error)|([1-9]+ exception)" /tmp/simpletest-result.txt && exit 1
+exit 0
