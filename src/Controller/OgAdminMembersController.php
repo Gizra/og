df17a3f405ee19bc68297ca0b1112470d5021b89
Justin Ludwig <justin@jludwig.net>
2017-02-01 13:59:38 -0800

    Adds membership type page to facilitate when multiple membership types exist. Additional cleanup as well.

--- src/Controller/OgAdminMembersController.php
+++ src/Controller/OgAdminMembersController.php
@@ -3,9 +3,14 @@
 namespace Drupal\og\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\Controller\EntityController;
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Link;
 use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\og\Entity\OgMembership;
 use Drupal\views\Views;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * OgAdminMembersController class.
@@ -32,6 +37,51 @@ class OgAdminMembersController extends ControllerBase {
   }
 
   /**
+   * Displays add membership links for available membership types.
+   *
+   * Returns default membership type if that's all that exists.
+   *
+   * @return array
+   *   A render array for a list of the node types that can be added; however,
+   *   if there is only one node type defined for the site, the function
+   *   will return the default add member form.
+   */
+  public function addPage(RouteMatchInterface $route_match) {
+    $bundles = $this->entityTypeManager()->getStorage('og_membership_type')->loadMultiple();
+    if ($bundles && count($bundles) == 1) {
+      $type = reset($types);
+      return $this->addForm($route_match);
+    }
+
+    $build = [
+      '#theme' => 'entity_add_list',
+      '#bundles' => [],
+    ];
+
+    $bundle_entity_type = $this->entityTypeManager()->getDefinition('og_membership_type');
+    $build['#cache']['tags'] = $bundle_entity_type->getListCacheTags();
+
+    $group_type_id = $route_match->getRouteObject()->getOption('_og_entity_type_id');
+
+    /** @var \Drupal\Core\Entity\EntityInterface $group */
+    $group = $route_match->getParameter($group_type_id);
+    $entity_type_id = $group->getEntityType()->id();
+
+    foreach ($bundles as $bundle_name => $bundle_info) {
+      $build['#bundles'][$bundle_name] = [
+        'label' => $bundle_info['name'],
+        'add_link' => Link::createFromRoute(
+          $bundle_info['name'],
+          "entity.$entity_type_id.og_admin_routes.add_membership",
+          [$entity_type_id => $group->id(), 'membership_type' => $bundle_name]
+        ),
+      ];
+    }
+
+    return $build;
+  }
+
+  /**
    * Provides the add member submission form.
    *
    * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
@@ -40,9 +90,13 @@ class OgAdminMembersController extends ControllerBase {
    * @return array
    *   The member add form.
    */
-  public function addMember(RouteMatchInterface $route_match) {
+  public function addForm(RouteMatchInterface $route_match) {
     /** @var \Drupal\og\Entity\OgMembershipType $membership_type */
     $membership_type = $route_match->getParameter('membership_type');
+    if ($membership_type === NULL) {
+      $membership_type = $this->entityTypeManager->getStorage('og_membership_type')->load('default');
+    }
+
     $group_type_id = $route_match->getRouteObject()->getOption('_og_entity_type_id');
 
     /** @var \Drupal\Core\Entity\EntityInterface $group */
--- src/Form/OgMembershipDeleteForm.php
+++ src/Form/OgMembershipDeleteForm.php
@@ -30,7 +30,7 @@ class OgMembershipDeleteForm extends ContentEntityDeleteForm {
     /** @var \Drupal\og\Entity\OgMembership $entity */
     $membership = $this->getEntity();
 
-    $this->logger('og')->notice("OG Membership: Deleted the @membership_type membership for the user uid: @uid to the group of the entity-type @group_type and ID: @gid", [
+    $this->logger('og')->notice("OG Membership: deleted the @membership_type membership for the user uid: @uid to the group of the entity-type @group_type and ID: @gid", [
       '@membership_type' => $membership->getType(),
       '@uid' => $membership->getUser()->id(),
       '@group_type' => $membership->getGroupEntityType(),
