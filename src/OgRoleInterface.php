7aae69589c1fe55a21415586d4133063fa15eb6f
Dan Braghis <dan.braghis@torchbox.com>
2017-02-21 16:14:51 +0000

    Add amitaibu/196

--- src/OgRoleInterface.php
+++ src/OgRoleInterface.php
@@ -126,6 +126,17 @@ interface OgRoleInterface {
    */
   public function setRoleType($role_type);
 
+  /*
+   * Returns whether or not a role can be changed.
+   *
+   * This will return FALSE for all roles except the default roles 'non-member'
+   * and 'member'.
+   *
+   * @return bool
+   *   Whether or not the role is locked.
+   */
+  public function isLocked();
+
   /**
    * Returns the role name.
    *
