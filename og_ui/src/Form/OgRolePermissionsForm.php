45eeb228873baa736fe6f80a210970a30182c903
Dan Braghis <dan.braghis@torchbox.com>
2017-03-07 17:31:02 +0000

    Fix PHP warnings

--- og_ui/src/Form/OgRolePermissionsForm.php
+++ og_ui/src/Form/OgRolePermissionsForm.php
@@ -33,8 +33,9 @@ class OgRolePermissionsForm extends OgPermissionsForm {
    *   The group bundle id.
    *
    * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   *   The role permission form title.
    */
-  public function titleCallback($entity_type, $bundle, $og_role) {
+  public function rolePermissionTitleCallback($entity_type, $bundle, $og_role) {
     $role = OgRole::load($og_role);
     return $this->t('@bundle roles - @role permissions', [
       '@bundle' => $this->entityTypeBundleInfo->getBundleInfo($entity_type)[$bundle]['label'],
@@ -45,10 +46,12 @@ class OgRolePermissionsForm extends OgPermissionsForm {
   /**
    * {@inheritdoc}
    *
-   * @param \Drupal\og\Entity\OgRole $group_role
+   * @param string $og_role
+   *   The group role id.
    */
   public function buildForm(array $form, FormStateInterface $form_state, $entity_type = '', $bundle = '', $og_role = '') {
-    $this->roles = [OgRole::load($og_role)];
+    $role = OgRole::load($og_role);
+    $this->roles = [$role->id() => $role];
 
     return parent::buildForm($form, $form_state, $entity_type, $bundle);
   }
