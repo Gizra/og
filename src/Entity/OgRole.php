88bf6834856ca4a5d984f5b11d56c048dd19b4e6
Pieter Frenssen <pieter@frenssen.be>
2017-05-13 21:13:36 +0200

    Factor out the code that checks if a role is a required default role so it can be reused.

--- src/Entity/OgRole.php
+++ src/Entity/OgRole.php
@@ -244,13 +244,20 @@ class OgRole extends Role implements OgRoleInterface {
   public function delete() {
     // The default roles are required. Prevent them from being deleted for as
     // long as the group still exists.
-    if (in_array($this->id(), [self::ANONYMOUS, self::AUTHENTICATED]) && $this->groupTypeManager()->isGroup($this->getGroupType(), $this->getGroupBundle())) {
+    if ($this->isDefaultRole() && $this->groupTypeManager()->isGroup($this->getGroupType(), $this->getGroupBundle())) {
       throw new OgRoleException('The default roles "non-member" and "member" cannot be deleted.');
     }
 
     // Reset access cache, as the role is no longer present.
     $this->ogAccess()->reset();
     parent::delete();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function isDefaultRole() {
+    return in_array($this->getName(), [self::ANONYMOUS, self::AUTHENTICATED]);
   }
 
   /**
