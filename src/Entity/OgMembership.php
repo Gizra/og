514e22c14bbf700746ea975d800215bf31a0f70a
Joachim Noreiko <joachim@107701.no-reply.drupal.org>
2017-09-01 13:46:59 +0100

    Fixed missing import.

--- src/Entity/OgMembership.php
+++ src/Entity/OgMembership.php
@@ -9,6 +9,7 @@ use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Field\BaseFieldDefinition;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\Core\Session\AccountInterface;
+use Drupal\user\UserInterface;
 use Drupal\og\Og;
 use Drupal\og\OgMembershipInterface;
 use Drupal\og\OgRoleInterface;
