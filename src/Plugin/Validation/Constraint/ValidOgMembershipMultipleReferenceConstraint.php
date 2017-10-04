365da2f03f0ce9b7a81129811629095433da54ff
Roy Segall <roy.segall@gizra.com>
2017-10-04 18:22:43 +0300

    Minor fixes.

--- src/Plugin/Validation/Constraint/ValidOgMembershipMultipleReferenceConstraint.php
+++ src/Plugin/Validation/Constraint/ValidOgMembershipMultipleReferenceConstraint.php
@@ -7,13 +7,13 @@ use Symfony\Component\Validator\Constraint;
 /**
  * Entity Reference valid references constraint.
  *
- * Make sure that when user that can only post content inside a group populated
- * one of the audience fields. After the fields are populated the constraint for
- * checking valid references will make sure the values are OK.
+ * Make sure that when a user that can only post content inside a group
+ * populated one of the audience fields. After the fields are populated the
+ * constraint for checking valid references will make sure the values are OK.
  *
  * @Constraint(
  *   id = "ValidOgMembershipMultipleReference",
- *   label = @Translation("Organic Groups valid reference", context = "Validation")
+ *   label = @Translation("Organic Groups valid references", context = "Validation")
  * )
  */
 class ValidOgMembershipMultipleReferenceConstraint extends Constraint {
