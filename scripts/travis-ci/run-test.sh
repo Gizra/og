7eb8fd6a408aca236829523f6057b71dd8da8782
Aron Novak <aron@gizra.com>
2018-05-29 10:47:46 +0200

    initial idea to execute og_ui tests too

--- scripts/travis-ci/run-test.sh
+++ scripts/travis-ci/run-test.sh
@@ -3,6 +3,8 @@
 # Run either PHPUnit tests or PHP_CodeSniffer tests on Travis CI, depending
 # on the passed in parameter.
 
+TEST_DIRS=($MODULE_DIR/tests $MODULE_DIR/og_ui/tests)
+
 case "$1" in
     PHP_CodeSniffer)
         cd $MODULE_DIR
@@ -14,12 +16,14 @@ case "$1" in
     8.3.x)
         ln -s $MODULE_DIR $DRUPAL_DIR/modules/og
         cd $DRUPAL_DIR
-        ./vendor/bin/phpunit -c ./core/phpunit.xml.dist $MODULE_DIR/tests
-        exit $?
+        for i in ${TEST_DIRS[@]}; do
+          ./vendor/bin/phpunit -c ./core/phpunit.xml.dist $i || exit 1
+        done
         ;;
     *)
         ln -s $MODULE_DIR $DRUPAL_DIR/modules/og
         cd $DRUPAL_DIR
-        ./vendor/bin/phpunit -c ./core/phpunit.xml.dist --exclude-group=console $MODULE_DIR/tests
-        exit $?
+        for i in ${TEST_DIRS[@]}; do
+          ./vendor/bin/phpunit -c ./core/phpunit.xml.dist --exclude-group=console $i || exit 1
+        done
 esac
