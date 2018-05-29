c873d57e8bc000443be67dd3796ac9e36f263d0b
Aron Novak <aron@gizra.com>
2018-05-29 10:56:39 +0200

    mysql to ramdisk attempt

--- scripts/travis-ci/run-test.sh
+++ scripts/travis-ci/run-test.sh
@@ -3,6 +3,14 @@
 # Run either PHPUnit tests or PHP_CodeSniffer tests on Travis CI, depending
 # on the passed in parameter.
 
+mysql_to_ramdisk() {
+  echo " > Move MySQL datadir to RAM disk."
+  sudo service mysql stop
+  sudo mv /var/lib/mysql /var/run/tmpfs
+  sudo ln -s /var/run/tmpfs /var/lib/mysql
+  sudo service mysql start
+}
+
 case "$1" in
     PHP_CodeSniffer)
         cd $MODULE_DIR
@@ -12,12 +20,14 @@ case "$1" in
         ;;
     # Drupal console only works on Drupal 8.3.x.
     8.3.x)
+        mysql_to_ramdisk
         ln -s $MODULE_DIR $DRUPAL_DIR/modules/og
         cd $DRUPAL_DIR
         ./vendor/bin/phpunit -c ./core/phpunit.xml.dist $MODULE_DIR/tests
         exit $?
         ;;
     *)
+        mysql_to_ramdisk
         ln -s $MODULE_DIR $DRUPAL_DIR/modules/og
         cd $DRUPAL_DIR
         ./vendor/bin/phpunit -c ./core/phpunit.xml.dist --exclude-group=console $MODULE_DIR/tests
