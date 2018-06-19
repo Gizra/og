549dea9e2da60c799cc4d1f595fbd7f009e35ac2
David Hernandez <davidhernandezruiz@gmail.com>
2018-06-19 19:09:20 +0200

    Removes code that is no longer necessary.

--- tests/src/Unit/OgAccessEntityTestBase.php
+++ tests/src/Unit/OgAccessEntityTestBase.php
@@ -8,7 +8,6 @@ use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\FieldableEntityInterface;
-use Drupal\Core\Field\EntityReferenceFieldItemList;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldStorageDefinitionInterface;
 use Drupal\og\OgGroupAudienceHelperInterface;
@@ -43,31 +42,18 @@ class OgAccessEntityTestBase extends OgAccessTestBase {
     $entity_type->isSubclassOf(FieldableEntityInterface::class)->willReturn(TRUE);
     $entity_type->id()->willReturn($entity_type_id);
 
-    // Some mocked groups that are referenced by the content entity.
-    $groups = [
-      $this->prophesize(ContentEntityInterface::class),
-      $this->prophesize(ContentEntityInterface::class),
-    ];
-
-    $og_audience = $this->prophesize(EntityReferenceFieldItemList::class);
-    $og_audience->referencedEntities()->willReturn($groups);
-
     $this->groupContentEntity = $this->prophesize(ContentEntityInterface::class);
     $this->groupContentEntity->id()->willReturn($entity_id);
     $this->groupContentEntity->bundle()->willReturn($bundle);
     $this->groupContentEntity->isNew()->willReturn(FALSE);
     $this->groupContentEntity->getEntityType()->willReturn($entity_type->reveal());
     $this->groupContentEntity->getEntityTypeId()->willReturn($entity_type_id);
-    $this->groupContentEntity->get(OgGroupAudienceHelperInterface::DEFAULT_FIELD)->willReturn($og_audience);
     $this->addCache($this->groupContentEntity);
 
     // If the group audience helper is asked if the group content entity has any
     // group audience fields, it is expected that this will return TRUE.
     $this->groupAudienceHelper->hasGroupAudienceField($entity_type_id, $bundle)
       ->willReturn(TRUE);
-
-    $this->groupAudienceHelper->getAllGroupAudienceFields($entity_type_id, $bundle)
-      ->willReturn([OgGroupAudienceHelperInterface::DEFAULT_FIELD => OgGroupAudienceHelperInterface::DEFAULT_FIELD]);
 
     // It is expected that a list of entity operation permissions is retrieved
     // from the permission manager so that the passed in permission can be
