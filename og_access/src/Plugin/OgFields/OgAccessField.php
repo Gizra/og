79f1a9efeef2ce7f216b199894e01ab508f61b45
Maarten Segers <MPParsley@users.noreply.github.com>
2019-04-09 14:43:31 +0200

    Update og_access/src/Plugin/OgFields/OgAccessField.php

--- og_access/src/Plugin/OgFields/OgAccessField.php
+++ og_access/src/Plugin/OgFields/OgAccessField.php
@@ -4,7 +4,6 @@ namespace Drupal\og_access\Plugin\OgFields;
 
 use Drupal\og\OgFieldBase;
 use Drupal\og\OgFieldsInterface;
-use Drupal\og_access\OgAccess;
 
 /**
  * Determine if group should use default roles and permissions.
