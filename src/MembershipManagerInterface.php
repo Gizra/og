d79f3eff05762172c11c30bf582dbdf8b19e8c3b
Maarten Segers <MPParsley@users.noreply.github.com>
2019-05-11 20:25:47 +0200

    Fixed documentation

--- src/MembershipManagerInterface.php
+++ src/MembershipManagerInterface.php
@@ -132,7 +132,7 @@ interface MembershipManagerInterface {
    * @param \Drupal\Core\Session\AccountInterface $user
    *   The user object.
    * @param string $membership_type
-   *   (optional) The membership type. Defaults to OG_MEMBERSHIP_TYPE_DEFAULT.
+   *   (optional) The membership type. Defaults to OgMembershipInterface::TYPE_DEFAULT.
    *
    * @return \Drupal\og\OgMembershipInterface
    *   The unsaved membership object.
