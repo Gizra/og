72338e410f3f80779b1171fe02f9b630ced14301
kbrodej <klemen.brodej@agiledrop.com>
2020-02-06 14:21:30 +0000

    Added proper method, added @codingStandartIgnore to avoid false positives, see https://www.drupal.org/project/coder/issues/3050416

--- src/Og.php
+++ src/Og.php
@@ -386,19 +386,19 @@ class Og {
    * @throws \Exception
    *   Thrown when the passed in field definition is not of a group audience
    *   field.
-   * @codingStandardsIgnoreStart
-   * @deprecated in og 8.x-1.0-alpha4 and is removed in og 8.x-1.0-alpha5.
+   *
+   * @deprecated in og:8.x-1.0-alpha4 and is removed from og:8.x-1.0-alpha5.
    *   Use
    *   \Drupal\Core\Entity\EntityReferenceSelection\SelectionPluginManager::getSelectionHandler()
    *   Instead.
-   *
+   * @codingStandardsIgnoreStart
    * @see https://github.com/Gizra/og/issues/580
    * @codingStandardsIgnoreEnd
    */
   public static function getSelectionHandler(FieldDefinitionInterface $field_definition, array $options = []) {
     // @codingStandardsIgnoreStart
-    @trigger_error('Og:getSelectionHandler() is deprecated in og 8.x-1.0-alpha4
-      and is removed in og 8.x-1.0-alpha5.
+    @trigger_error('Og:getSelectionHandler() is deprecated in og:8.x-1.0-alpha4
+      and is removed from og:8.x-1.0-alpha5.
       Use \Drupal\Core\Entity\EntityReferenceSelection\SelectionPluginManager::gegetSelectionHandler()
       instead. See https://github.com/Gizra/og/issues/580', E_USER_DEPRECATED
     );
