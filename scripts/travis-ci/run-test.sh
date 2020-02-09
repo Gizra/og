b25f0d1c569f32afdcb8b6d6117b9afc0ba00592
Maarten Segers <MPParsley@users.noreply.github.com>
2020-02-09 12:03:46 +0100

    Update run-test.sh

--- scripts/travis-ci/run-test.sh
+++ scripts/travis-ci/run-test.sh
@@ -27,7 +27,7 @@ case "$1" in
         EXIT=0
         for i in ${TEST_DIRS[@]}; do
           echo " > Executing tests from $i"
-          ./vendor/bin/phpunit -c ./core/phpunit.xml.dist $i || EXIT=1
+          ./vendor/bin/phpunit --coverage-clover "$TRAVIS_BUILD_DIR/build/logs/clover.xml" -c ./core/phpunit.xml.dist $i || EXIT=1
         done
         exit $EXIT
 esac
