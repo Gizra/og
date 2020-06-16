91a9000fc12605eceb1b09dcd02f88dcc1bbedea
Maarten Segers <MPParsley@users.noreply.github.com>
2020-06-16 07:27:43 +0200

    Update og_migrate/tests/src/Kernel/Migrate/OgMembershipTypeD7MigrateTest.php

--- og_migrate/tests/src/Kernel/Migrate/OgMembershipTypeD7MigrateTest.php
+++ og_migrate/tests/src/Kernel/Migrate/OgMembershipTypeD7MigrateTest.php
@@ -24,7 +24,7 @@ class OgMembershipTypeD7MigrateTest extends MigrateDrupal7TestBase {
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
 
     $this->installConfig(['og']);
