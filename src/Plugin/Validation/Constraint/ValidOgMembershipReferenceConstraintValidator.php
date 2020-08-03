9b2aaae8d3253d93ec8249f48570e168859b8048
Maarten Segers <MPParsley@users.noreply.github.com>
2020-08-03 14:37:48 +0200

    Inject services
    - Drupal\Core\Session\AccountInterface
    - Drupal\og\OgAccessInterface

--- src/Plugin/Validation/Constraint/ValidOgMembershipReferenceConstraintValidator.php
+++ src/Plugin/Validation/Constraint/ValidOgMembershipReferenceConstraintValidator.php
@@ -2,14 +2,54 @@
 
 namespace Drupal\og\Plugin\Validation\Constraint;
 
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Session\AccountInterface;
 use Drupal\og\Og;
+use Drupal\og\OgAccessInterface;
 use Symfony\Component\Validator\Constraint;
 use Symfony\Component\Validator\ConstraintValidator;
 
 /**
  * Checks if referenced entities are valid.
  */
-class ValidOgMembershipReferenceConstraintValidator extends ConstraintValidator {
+class ValidOgMembershipReferenceConstraintValidator extends ConstraintValidator implements ContainerInjectionInterface {
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The OG access service.
+   *
+   * @var \Drupal\og\OgAccessInterface
+   */
+  protected $ogAccess;
+
+  /**
+   * Constructs a ValidOgMembershipReferenceConstraintValidator object.
+   *
+   * @param \Drupal\Core\Session\AccountInterface $current_user
+   *   The current user.
+   * @param \Drupal\og\OgAccessInterface $og_access
+   *   The OG access service.
+   */
+  public function __construct(AccountInterface $current_user, OgAccessInterface $og_access) {
+    $this->currentUser = $current_user;
+    $this->ogAccess = $og_access;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInjectionInterface $container) {
+    return new static(
+      $container->get('current_user'),
+      $container->get('og.access')
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -40,8 +80,8 @@ class ValidOgMembershipReferenceConstraintValidator extends ConstraintValidator 
     $entity = $this->context->getRoot()->getValue();
 
     /** @var \Drupal\Core\Access\AccessResult $access */
-    $user = \Drupal::currentUser()->getAccount();
-    $access = \Drupal::service('og.access')->userAccessGroupContentEntityOperation('create', $group, $entity, $user);
+    $user = $this->currentUser->getAccount();
+    $access = $this->ogAccess->userAccessGroupContentEntityOperation('create', $group, $entity, $user);
 
     if ($access->isForbidden()) {
       $this->context->addViolation($constraint->notAllowedToPostInGroup, $params);
