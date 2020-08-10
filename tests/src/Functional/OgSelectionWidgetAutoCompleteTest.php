528025d59d8bb3057567446404162285111783f9
Maarten Segers <maarten.segers@digipolis.gent>
2020-08-10 12:34:44 +0200

    Fix strict types

--- tests/src/Functional/OgSelectionWidgetAutoCompleteTest.php
+++ tests/src/Functional/OgSelectionWidgetAutoCompleteTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\Tests\og\Functional;
 
 use Behat\Mink\Exception\ResponseTextException;
--- tests/src/Kernel/EntityReference/OgSelectionConfigurationFormTest.php
+++ tests/src/Kernel/EntityReference/OgSelectionConfigurationFormTest.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace Drupal\Tests\og\Kernel\EntityReference;
 
 use Drupal\Core\Form\FormState;
