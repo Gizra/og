5a68c33426348d0712dcd94d66b6aea0c32e3d6d
pwolanin <pwolanin@49851.no-reply.drupal.org>
2021-06-28 14:18:31 -0400

    Fix for call to undefined method, see https://www.drupal.org/node/2850048

--- src/Controller/OgAutocompleteController.php
+++ src/Controller/OgAutocompleteController.php
@@ -86,7 +86,7 @@ class OgAutocompleteController extends ControllerBase {
     // Get the typed string from the URL, if it exists.
     if ($input = $request->query->get('q')) {
       $typed_string = Tags::explode($input);
-      $typed_string = Unicode::strtolower(array_pop($typed_string));
+      $typed_string = mb_strtolower(array_pop($typed_string));
 
       // Selection settings are passed in as a hashed key of a serialized array
       // stored in the key/value store.
