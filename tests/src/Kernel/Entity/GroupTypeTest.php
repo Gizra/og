3792bc4d14c8e409298499f2399e7790ac4a04cd
pwolanin <pwolanin@49851.no-reply.drupal.org>
2021-07-13 10:38:38 -0400

    Block og_membership from being a group or group content

--- tests/src/Kernel/Entity/GroupTypeTest.php
+++ tests/src/Kernel/Entity/GroupTypeTest.php
@@ -40,21 +40,41 @@ class GroupTypeTest extends KernelTestBase {
    * Test creation and deletion of a group type.
    */
   public function testGroupType() {
+    // Even if we set the og_membership into the config it should not
+    // be recognized as a group type.
+    $editable = $this->config('og.settings');
+    $groups = $editable->get('groups');
+    $groups['og_membership'][] = 'og_membership';
+    $editable->set('groups', $groups);
+    $editable->save();
     // Create a content type.
     /** @var \Drupal\node\NodeTypeInterface $group_type */
     $group_type = NodeType::create(['type' => 'group', 'name' => 'Group']);
     $group_type->save();
 
-    // Initially it should not be a group.
+    // Initially the node type should not be a group.
     $this->assertFalse($this->groupTypeManager->isGroup('node', 'group'));
 
     // Turn it into a group.
     $this->groupTypeManager->addGroup('node', 'group');
     $this->assertTrue($this->groupTypeManager->isGroup('node', 'group'));
 
+    // The membership entity should not be a group despite being in config.
+    $this->assertFalse($this->groupTypeManager->isGroup('og_membership', 'og_membership'));
+
+   // Verify that the config still contains og_membership.
+    $editable = $this->config('og.settings');
+    $groups = $editable->get('groups');
+    $this->assertFalse(empty($groups['og_membership']));
+    $this->assertFalse(empty($groups['node']));
+
     // Delete the content type. It should no longer be a group.
     $group_type->delete();
     $this->assertFalse($this->groupTypeManager->isGroup('node', 'group'));
+
+    // Adding og_membership as a group type is not possible.
+    $this->expectException(\InvalidArgumentException::class);
+    $this->groupTypeManager->addGroup('og_membership', 'og_membership');
   }
 
 }
