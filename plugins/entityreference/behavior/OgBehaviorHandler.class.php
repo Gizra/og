cfc6eb9d7a82ac9bc56e0cbba0720b5f9c3c98fa
onesixromcom <16732145+onesixromcom@users.noreply.github.com>
2022-05-03 13:42:32 +0300

    Fix deletion of Blocked and Pending users

--- plugins/entityreference/behavior/OgBehaviorHandler.class.php
+++ plugins/entityreference/behavior/OgBehaviorHandler.class.php
@@ -78,7 +78,8 @@ class OgBehaviorHandler extends EntityReference_BehaviorHandler_Abstract {
     if (!empty($entity->delete_og_membership)) {
       // Delete all OG memberships related to this entity.
       $og_memberships = array();
-      foreach (og_get_entity_groups($entity_type, $entity) as $group_type => $ids) {
+      $groups = og_get_entity_groups($entity_type, $entity, array(OG_STATE_ACTIVE, OG_STATE_PENDING, OG_STATE_BLOCKED));
+      foreach ($groups as $group_type => $ids) {
         $og_memberships = array_merge($og_memberships, array_keys($ids));
       }
       if ($og_memberships) {
